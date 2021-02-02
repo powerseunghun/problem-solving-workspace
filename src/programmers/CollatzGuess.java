68515bc0bade1e3de8c5d5b1addec80cc650807c
안승훈 <size411@naver.com>
2021-02-02 22:55:26 +0900

    CollatzGuess & FindKimSoeBang

---
+++ src/programmers/CollatzGuess.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class CollatzGuess {
+	public int solution(long n) {
+		int answer = 0;
+		
+		for (int i = 0; i < 500 && n != 1; i++) {
+			if (n % 2 == 0) {
+				n /= 2;
+			}
+			else {
+				n = n * 3 + 1;
+			}
+			answer++;
+		}
+		if (answer >= 500) answer = -1;
+		return answer;
+	}
+	public static void main(String[] args) {
+		System.out.println(new CollatzGuess().solution(626331));
+	}
+}
---
+++ src/programmers/FindKimSoeBang.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class FindKimSoeBang {
+	public String solution(String[] seoul) {
+		String answer = "김서방은 ";
+		for (int i = 0; i < seoul.length; i++) {
+			if (seoul[i].equals("Kim")) {
+				answer += i;
+			}
+		}
+		
+		return answer + "에 있다";
+	}
+	public static void main(String[] args) {
+		
+	}
+}
