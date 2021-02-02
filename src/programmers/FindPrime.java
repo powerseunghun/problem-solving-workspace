beccedfd3c5e1d852b17d4451bf4043f035aa22b
안승훈 <size411@naver.com>
2021-02-02 23:08:46 +0900

    EratosthenesSieve

---
+++ src/programmers/FindPrime.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class FindPrime {
+	public int solution(int n) {
+		int answer = 0;
+		int[] prime = new int[1000000];
+		
+		for (int i = 2; i <= n; i++) {
+			prime[i] = i;
+		}
+		
+		for (int i = 2; i <= n; i++) {
+			if (prime[i] == 0) continue;
+			for (int j = i + i; j <= n; j += i) {
+				prime[j] = 0;
+			}
+		}
+		for (int i = 2; i <= n; i++) {
+			if (prime[i] != 0) answer++;
+		}
+		
+		return answer;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
