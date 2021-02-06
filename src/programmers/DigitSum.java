4cbd9305fc1765bb3ecd7ca839398fe337b0ddc2
안승훈 <size411@naver.com>
2021-02-06 23:05:25 +0900

    DigitSum & MakeWeirdString

---
+++ src/programmers/DigitSum.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class DigitSum {
+	public int solution(int n) {
+		int answer = 0;
+		while (n != 0) {
+			
+		}
+		return answer;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/programmers/MakeWeirdString.java
@@ -0,0 +1,30 @@
+package programmers;
+
+public class MakeWeirdString {
+	public String solution(String s) {
+		String answer = "";
+		String tmp = "";
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == ' ') {
+				count = 0;
+				answer += " ";
+				continue;
+			}
+			switch(count % 2) {
+			case 0:
+				tmp = (s.charAt(i) + "").toUpperCase();
+				break;
+			case 1:
+				tmp = (s.charAt(i) + "").toLowerCase();
+				break;
+			}
+			count++;
+			answer += tmp;
+		}
+		return answer;
+	}
+	public static void main(String[] args) {
+		System.out.println(new MakeWeirdString().solution("try hello world"));
+	}
+}
