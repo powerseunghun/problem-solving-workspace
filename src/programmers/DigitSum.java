deda14818759769b03ceab46b25e09e284c7b8c3
안승훈 <size411@naver.com>
2021-02-06 23:22:17 +0900

    DigitSum modify

--- src/programmers/DigitSum.java
+++ src/programmers/DigitSum.java
@@ -4,7 +4,8 @@ public class DigitSum {
 	public int solution(int n) {
 		int answer = 0;
 		while (n != 0) {
-			
+			answer += n % 10;
+			n /= 10;
 		}
 		return answer;
 	}
---
+++ src/programmers/ReverseNumberMakeArr.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.ArrayList;
+
+public class ReverseNumberMakeArr {
+	public ArrayList<Integer> solution(long n) {
+		StringBuffer s = new StringBuffer(String.valueOf(n)).reverse();
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < s.length(); i++) {
+			list.add(s.charAt(i) - 48);
+		}
+		return list;
+	}
+//	public int[] solution(int n) {
+//		int[] answer = new int[(n + "").length()];
+//		
+//		for (int i = 0; i < answer.length; i++) {
+//			answer[i] = n % 10;
+//			n /= 10;
+//		}
+//		return answer;
+//	}
+	public static void main(String[] args) {
+//		System.out.println(Arrays.toString(new ReverseNumberMakeArr().solution(12345)));
+		System.out.println(new ReverseNumberMakeArr().solution(12345));
+	}
+}
