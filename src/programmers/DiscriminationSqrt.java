6b2fbb4f9995a7cc79ccd32900d58ae06454edd0
안승훈 <size411@naver.com>
2021-02-07 20:06:14 +0900

    DiscriminationSqrt

---
+++ src/programmers/DiscriminationSqrt.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class DiscriminationSqrt {
+	public long solution(long n) {
+		long answer = 0;
+		double sqrt = Math.sqrt(n);
+		if (sqrt - (int)sqrt == 0) {
+			answer = (long) ((Math.sqrt(n) + 1) * (Math.sqrt(n) + 1));
+		}
+		else answer = -1;
+		
+		return answer;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
