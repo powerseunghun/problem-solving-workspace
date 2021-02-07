e65e906f2a00e4e7e10c333a4824e1bc0fd9e6d1
안승훈 <size411@naver.com>
2021-02-07 20:37:11 +0900

    EvenOdd

--- src/Programmers/level1/EvenOdd.java
+++ src/Programmers/level1/EvenOdd.java
@@ -1,5 +1,12 @@
 package Programmers.level1;
 
 public class EvenOdd {
-
+	public String solution(int num) {
+		String answer = "";
+		answer = num % 2 == 0 ? "Even" : "Odd";
+		return answer;
+	}
+	public static void main(String[] args) {
+		
+	}
 }
