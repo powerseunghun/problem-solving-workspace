d9c72db579c82dcd0e512a324cbd60eed351268d
안승훈 <size411@naver.com>
2021-02-07 19:53:51 +0900

    StringDescending2

---
+++ src/programmers/StringDescending2.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class StringDescending2 {
+	public long solution(long n) {
+		long answer = 0;
+		long[] count = new long[10];
+		String str = "";
+		
+		while (n != 0) {
+			count[(int) (n % 10)]++;
+			n /= 10;
+		}
+		for (int i = 9; i >= 0; i--) {
+			if (count[i] != 0) {
+				for (int j = 0; j < count[i]; j++) {
+					str += i + "";
+				}
+			}
+		}
+		answer = Long.parseLong(str);
+		return answer;
+	}
+	public static void main(String[] args) {
+		System.out.println(new StringDescending2().solution(118372));
+	}
+}
