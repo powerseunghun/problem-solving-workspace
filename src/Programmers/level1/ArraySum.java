5e32c6257659a2e9d21d61309721439015b1e64d
powerseunghun <size411@naver.com>
2021-02-11 21:21:47 +0900

    ArraySum

--- src/Programmers/level1/ArraySum.java
+++ src/Programmers/level1/ArraySum.java
@@ -11,7 +11,6 @@ public class ArraySum {
         }
         return answer;
     }
-	
 	public static void main(String[] args) {
 		
 	}
---
+++ src/Programmers/level1/RectangleStar.java
@@ -0,0 +1,5 @@
+package Programmers.level1;
+
+public class RectangleStar {
+
+}
