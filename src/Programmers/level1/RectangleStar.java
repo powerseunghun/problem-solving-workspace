981b2f7a78c95aee4a4c3efc0c3438efba1b23e0
powerseunghun <size411@naver.com>
2021-02-11 21:24:58 +0900

    RectangleStar

--- src/Programmers/level1/RectangleStar.java
+++ src/Programmers/level1/RectangleStar.java
@@ -1,5 +1,17 @@
 package Programmers.level1;
 
-public class RectangleStar {
+import java.util.Scanner;
 
+public class RectangleStar {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
