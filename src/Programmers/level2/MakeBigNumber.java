d266537577c105f06d4d18c6b0686639402d9449
powerseunghun <size411@naver.com>
2021-02-18 21:55:11 +0900

    MakeBigNumber ing

---
+++ src/Programmers/level2/MakeBigNumber.java
@@ -0,0 +1,36 @@
+package Programmers.level2;
+
+public class MakeBigNumber {
+	public String solution(String number, int k) {
+        String answer = "", subs ="";
+        int loop = number.length() - k, max = 0;
+		int maxIdx = 0, subIdx = loop - 1;
+        for (int i = 0; i < loop; i++) {
+        	max = 0;
+        	subs = number.substring(0, number.length() - subIdx + i);
+        	System.out.println("sub : " + subs);
+        	for (int j = 0; j < subs.length(); j++) {
+        		if (subs.charAt(j) - 48 > max) {
+        			max = subs.charAt(j) - 48;
+        			maxIdx = j;
+        		}
+        		
+        	}
+        	answer += max;
+        	String tmp = number.toString();
+        	number = "";
+        	for (int j = 0; j < tmp.length(); j++) {
+        		if (j == maxIdx) continue;
+        		else number += tmp.charAt(j);
+        	}
+        }
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+		System.out.println(new MakeBigNumber().solution("4177252841", 4));
+		
+//		System.out.println('1' - 48);
+		
+	}
+}
