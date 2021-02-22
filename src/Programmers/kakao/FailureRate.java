1db356f4a297b5442cdeaf9e6459d8afaddbb850
안승훈 <size411@naver.com>
2021-02-23 01:05:05 +0900

    FailureRate ing2

--- src/Programmers/kakao/FailureRate.java
+++ src/Programmers/kakao/FailureRate.java
@@ -2,34 +2,46 @@ package Programmers.kakao;
 
 public class FailureRate {
 	public int[] solution(int N, int[] stages) {
-        int[] answer = {};
+        int[] answer = new int[N];
         int[] personInStage = new int[N + 2];
-        int stage = 1, count = 0;
+        double[] failureRate = new double[N], failureRateCopy = new double[N];
+        int partSum = 0;
         
         for (int i = 0; i < stages.length; i++) {
         	personInStage[stages[i]]++;
         }
-        for (int i = 0; i < personInStage.length; i++) {
-        	System.out.print(personInStage[i] + " ");
-        }
-        System.out.println();
-        int partSum = 0;
-        for (int i = 1; i < personInStage.length - 1; i++) {
-        	for (int j = 1; j <= i; j++) {
+        for (int i = 0; i < personInStage.length - 2; i++) {
+        	partSum = 0;
+        	for (int j = 0; j <= i; j++) {
         		partSum += personInStage[j];
         	}
-        	System.out.println(partSum);
-        	// total - partSum
-        	partSum = 0;
+        	int base = stages.length - partSum;
+        	failureRate[i] = base == 0 ? 0.0 : (double)personInStage[i+1] / base;
         }
-//        for (int i = 0 ; i < personInStage.length; i++) {
-//        }
-        
+        failureRateCopy = failureRate.clone();
+        for (int i = 0; i < failureRateCopy.length-1; i++) {
+        	for (int j = 0; j < failureRateCopy.length - (i + 1); j++) {
+        		if (failureRateCopy[j] < failureRateCopy[j + 1]) {
+        			double tmp = failureRateCopy[j];
+        			failureRateCopy[j] = failureRateCopy[j+1];
+        			failureRateCopy[j+1] = tmp;
+        		}
+        	}
+        }
+        for (int i = 0; i < failureRateCopy.length; i++) {
+        	for (int j = 0; j < failureRate.length; j++) {
+        		if (failureRateCopy[i] == failureRate[j]) {
+        			answer[i] = j + 1;
+        			failureRate[j] = -1.0;
+        			break;
+        		}
+        	}
+        }
         return answer;
     }
 	public static void main(String[] args) {
 		int N = 5;
-		int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
+		int[] stages = {1, 2, 2, 1, 3};
 		new FailureRate().solution(N, stages);
 	}
 }
