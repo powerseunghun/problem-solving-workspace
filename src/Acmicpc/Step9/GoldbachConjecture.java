13eb57ed6a148cc166417258e8b06d25c18bd6f1
안승훈 <size411@naver.com>
2021-03-02 22:16:13 +0900

    Goldbach's Conjecture

---
+++ src/Acmicpc/Step9/GoldbachConjecture.java
@@ -0,0 +1,37 @@
+package Acmicpc.Step9;
+
+import java.util.Scanner;
+
+public class GoldbachConjecture {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T, n, a, b;
+		int[] arr = null;
+		
+		T = sc.nextInt();
+		
+		for (int i = 0; i < T; i++) {
+			n = sc.nextInt();
+			a = n / 2;
+			b = n / 2;
+			arr = new int[n + 1];
+			
+			for (int j = 2; j <= Math.sqrt(n); j++) {
+				for (int k = j * j; k <= n; k += j) {
+					arr[k] = 1;
+				}
+			}
+			
+			while(true) {
+				if ((arr[a] == 0 && arr[b] == 0) && a + b == n) {
+					System.out.println(a + " " + b);
+					break;
+				}
+				else {
+					a--;
+					b++;
+				}
+			}
+		}
+	}
+}
