206dfbc953678559f7ba35b7dd296a3fccbbd210
안승훈 <size411@naver.com>
2021-03-02 22:54:18 +0900

    EscapeFromeRectangle1085

--- src/Acmicpc/Step9/EscapeFromRectangle1085.java
+++ src/Acmicpc/Step9/EscapeFromRectangle1085.java
@@ -6,10 +6,7 @@ public class EscapeFromRectangle1085 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int x, y, w, h, tmp1, tmp2, answer;
-		x = sc.nextInt();
-		y = sc.nextInt();
-		w = sc.nextInt();
-		h = sc.nextInt();
+		x = sc.nextInt(); y = sc.nextInt(); w = sc.nextInt(); h = sc.nextInt();
 		tmp1 = Math.abs(w - x) >= Math.abs(0 - x) ? Math.abs(0 - x) : Math.abs(w - x);
 		tmp2 = Math.abs(h - y) >= Math.abs(0 - y) ? Math.abs(0 - y) : Math.abs(h - y);
 		answer = tmp1 >= tmp2 ? tmp2 : tmp1;
