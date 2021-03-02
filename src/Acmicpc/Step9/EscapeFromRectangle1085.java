905ae5d6586d81b9d1f1d552f208999eafff4b1c
안승훈 <size411@naver.com>
2021-03-02 22:50:13 +0900

    EscapeFromRectangle1085

--- src/Acmicpc/Step9/EscapeFromRectangle1085.java
+++ src/Acmicpc/Step9/EscapeFromRectangle1085.java
@@ -2,7 +2,7 @@ package Acmicpc.Step9;
 
 import java.util.Scanner;
 
-public class EscapeFromRectangle {
+public class EscapeFromRectangle1085 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int x, y, w, h, tmp1, tmp2, answer;
@@ -12,8 +12,8 @@ public class EscapeFromRectangle {
 		h = sc.nextInt();
 		tmp1 = Math.abs(w - x) >= Math.abs(0 - x) ? Math.abs(0 - x) : Math.abs(w - x);
 		tmp2 = Math.abs(h - y) >= Math.abs(0 - y) ? Math.abs(0 - y) : Math.abs(h - y);
-		
 		answer = tmp1 >= tmp2 ? tmp2 : tmp1;
+		
 		System.out.println(answer);
 	}
 }
