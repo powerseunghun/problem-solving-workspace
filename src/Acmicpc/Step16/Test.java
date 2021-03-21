dd419fc25bc9b56edb14562114e8dd5f47428bb6
안승훈 <size411@naver.com>
2021-03-21 18:22:23 +0900

    Night Check 2 fail

---
+++ src/Acmicpc/Step16/Test.java
@@ -0,0 +1,19 @@
+package Acmicpc.Step16;
+
+import java.io.IOException;
+
+public class Test {
+	public static void main(String[] args) throws IOException {
+//		System.out.printf("%c\n", System.in.read());
+//		System.out.printf("%c\n", System.in.read());
+//		System.out.printf("%c\n", System.in.read());
+		
+		for (int i = System.in.read(); i >= 'A'; i = System.in.read()) {
+			System.out.printf("%c\n", i);
+		}
+		
+//		for (int i = 77; i >= 'A'; i = 10) {
+//			System.out.println("H");
+//		}
+  	}
+}
