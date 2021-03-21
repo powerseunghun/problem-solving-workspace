c8a6c52c80eba863658b0fd7f6a5ddfc10d3437e
안승훈 <size411@naver.com>
2021-03-21 16:03:17 +0900

    GasStation & MissingParentheses

---
+++ src/Acmicpc/Step15/MissingParentheses1541.java
@@ -0,0 +1,33 @@
+package Acmicpc.Step15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MissingParentheses1541 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = null;
+		int sum = 0;
+		String[] includePlus = str.split("-");
+		
+		for (int i = 0; i < includePlus.length; i++) {
+			if (includePlus[i].contains("+")) {
+				st = new StringTokenizer(includePlus[i], "+");
+				sum = 0;
+				while(st.hasMoreTokens()) {
+					sum += Integer.parseInt(st.nextToken());
+					includePlus[i] = sum + "";
+				}
+			}
+		}
+		sum = Integer.parseInt(includePlus[0]);
+		for (int i = 1; i < includePlus.length; i++) {
+			sum -= Integer.parseInt(includePlus[i]);
+		}
+		
+		System.out.println(sum);
+	}
+}
