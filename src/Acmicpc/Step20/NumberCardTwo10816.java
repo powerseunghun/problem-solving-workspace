e4ed66192bd965d42330115ce64f85505333831a
안승훈 <size411@naver.com>
2021-03-28 00:47:36 +0900

    l_bound, u_bound implements ing

---
+++ src/Acmicpc/Step20/NumberCardTwo10816.java
@@ -0,0 +1,15 @@
+package Acmicpc.Step20;
+
+public class NumberCardTwo10816 {
+	// lower_bound, upper_bound implements q
+	public static int lower() {
+		return 0;
+	}
+	
+	public static int upper() {
+		return 0;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
