d1ce1e451e9d8d2ebf71363808c5923cdd87d0b2
안승훈 <size411@naver.com>
2021-04-25 19:13:15 +0900

    ws restore & resolve

---
+++ src/CodeUp/Thousandth/One/Print1001.java
@@ -0,0 +1,7 @@
+package CodeUp.Thousandth.One;
+
+public class Print1001 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
---
+++ src/CodeUp/Thousandth/One/Print1007.java
@@ -0,0 +1,7 @@
+package CodeUp.Thousandth.One;
+
+public class Print1007 {
+	public static void main(String[] args) {
+		System.out.println("\"C:\\Download\\hello.cpp\"");
+	}
+}
