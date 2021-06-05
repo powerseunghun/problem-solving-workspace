313d2d43b35311c024143fe5b3b54c21b88d146d
anseunghun <size411@naver.com>
2021-06-05 23:32:52 +0900

    Tree Order

---
+++ src/CodeUp/ThreeThousandth/Two/TreePostorder3203.java
@@ -0,0 +1,23 @@
+package CodeUp.ThreeThousandth.Two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class TreePostorder3203 {
+	static void inOrder(String str, int idx) {
+		if (idx > str.length()) return;
+		inOrder(str, 2 * idx);
+		inOrder(str, 2 * idx + 1);
+		System.out.print(str.charAt(idx-1));
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		char[] parent = new char[str.length()];
+		Arrays.fill(parent, ' ');
+		
+		inOrder(str, 1);
+	}
+}
