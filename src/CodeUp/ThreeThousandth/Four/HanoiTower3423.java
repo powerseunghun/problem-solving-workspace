f02e406f6d1859bb59aeeda62b642dcf34bb9e30
anseunghun <size411@naver.com>
2021-06-06 17:28:21 +0900

    HanoiTower 4 Peg

---
+++ src/CodeUp/ThreeThousandth/Four/HanoiTower3423.java
@@ -0,0 +1,23 @@
+package CodeUp.ThreeThousandth.Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HanoiTower3423 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()), sum = 1, weight = 2, count = 0, countLimit = 2;
+		for (int i = 1; i < N; i++) {
+			sum += weight;
+			count++;
+			if (count == countLimit) {
+				count = 0;
+				weight *= 2;
+				countLimit += 1;
+			}
+		}
+		
+		System.out.println(sum);
+	}
+}
