e630eaab883fbd90dca31f5d13c58a5de57187a1
anseunghun <size411@naver.com>
2021-07-04 19:30:49 +0900

    IntelligenceTrain ver2

---
+++ src/CodeUp/FourThousandth/Seven/IntelligenceTrain4716.java
@@ -0,0 +1,23 @@
+package CodeUp.FourThousandth.Seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class IntelligenceTrain4716 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String tmp = null;
+		int sum = 0, out = 0, in = 0, max = 0;
+		
+		for (int i = 0; i < 10; i++) {
+			tmp = br.readLine();
+			out = Integer.parseInt(tmp.split(" ")[0]);
+			in = Integer.parseInt(tmp.split(" ")[1]);
+			sum -= out;
+			sum += in;
+			max = Math.max(max, sum);
+		}
+		System.out.println(max);
+	}
+}
