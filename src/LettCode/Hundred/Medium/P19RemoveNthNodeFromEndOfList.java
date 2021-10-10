f5cc538326eebbea9b17d867e8b127f252e248d3
anseunghun <size411@naver.com>
2021-10-10 18:32:17 +0900

    LeetCode Medium Problem LinkedList Remove Two Pointer Approach

---
+++ src/LettCode/Hundred/Medium/P19RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,27 @@
+package LettCode.Hundred.Medium;
+
+public class P19RemoveNthNodeFromEndOfList {
+    static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        
+        for (int i = 0; i < n; i++) {
+        	if (p2.next == null) {
+        		head = i == (n-1) ? head.next : head;
+        		return head;
+        	}
+        	p2 = p2.next;
+        }
+        while (p2.next != null) {
+        	p1 = p1.next;
+        	p2 = p2.next;
+        }
+        if (p1.next != null) {
+        	p1.next = p1.next.next;
+        }
+        return head;
+    }
+	public static void main(String[] args) {
+		
+	}
+}
