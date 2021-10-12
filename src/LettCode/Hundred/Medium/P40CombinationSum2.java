6ec5e7dbcd6ff8bafc1f3d46e445682d46c82cb9
anseunghun <size411@naver.com>
2021-10-12 18:29:03 +0900

    LeetCode Problem CombinationSum2 TLE need improve

---
+++ src/LettCode/Hundred/Medium/P40CombinationSum2.java
@@ -0,0 +1,56 @@
+package LettCode.Hundred.Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class P40CombinationSum2 {
+	static List<List<Integer>> res = new ArrayList<>();
+	static Set<String> check = new HashSet<>();
+	static void search(int sum, int target, int idx, int[] candidates, List<Integer> list) {
+		if (sum >= target) {
+			if (sum == target) {
+				String str = "";
+				for (int i = 0; i < list.size(); i++) {
+					str += list.get(i);
+				}
+				if (!check.contains(str)) res.add(list);
+				check.add(str);
+			}
+			return;
+		}
+		
+		for (int i = idx; i < candidates.length; i++) {
+			List<Integer> transList = new ArrayList<>();
+			for (int j = 0; j < list.size(); j++) {
+				transList.add(list.get(j));
+			}
+			transList.add(candidates[i]);
+			search(sum+candidates[i], target, i+1, candidates, transList);
+		}
+	}
+    static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    	Arrays.sort(candidates);
+    	res.clear();
+    	check.clear();
+    	search(0, target, 0, candidates, new ArrayList<>());
+    	
+    	return res;
+    }
+	public static void main(String[] args) {
+		int[] candidates = { 2,5,2,1,2 };
+		int target = 5;
+		List<Integer> l = new ArrayList<>();
+		combinationSum2(candidates, target);
+		
+		for (int i = 0; i < res.size(); i++) {
+			List<Integer> t = res.get(i);
+			for (int j = 0; j < t.size(); j++) {
+				System.out.print(t.get(j) + " ");
+			}
+			System.out.println();
+		}
+	}
+}
