9e187fdf1272e844ab514f723686730a9edb5cbd
anseunghun <size411@naver.com>
2021-10-19 21:31:06 +0900

    DP Problem add..

---
+++ src/Acmicpc/DynamicProgramming/Thousandth/Nine/IntegerTriangle1932.java
@@ -0,0 +1,41 @@
+package Acmicpc.DynamicProgramming.Thousandth.Nine;
+
+import java.util.Scanner;
+
+public class IntegerTriangle1932 {
+	public static void main(String[] args) {
+		int[][] arr = null;
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt(), max = 0;
+		arr = new int[n][n];
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		for (int i = 1; i < n ; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (j == 0) {
+					arr[i][j] += arr[i-1][j];
+				}
+				else if (j == i) {
+					arr[i][j] += arr[i-1][j-1];
+				}
+				else {
+					arr[i][j] += Math.max(arr[i-1][j-1], arr[i-1][j]);
+				}
+				
+				if (i == (n - 1)) {
+					max = arr[i][j] >= max ? arr[i][j] : max;
+				}
+			}
+		}
+		if(n == 1) {
+			System.out.println(arr[0][0]);
+		}
+		else System.out.println(max);
+	}
+}
