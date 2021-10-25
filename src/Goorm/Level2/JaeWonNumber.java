540a6226da644843f49e5096299f476000c1da6f
anseunghun <size411@naver.com>
2021-10-25 20:36:12 +0900

    Goorm L2 Problem JaeWonNumber Memoization

---
+++ src/Goorm/Level2/JaeWonNumber.java
@@ -0,0 +1,22 @@
+package Goorm.Level2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class JaeWonNumber {
+	static long[] arr = new long[31];
+	
+	static long getJaeWonNumber(int N) {
+		if (arr[N] != 0) return arr[N];
+		if (N == 1) return arr[N] = 3;
+		else return arr[N] = (long)(getJaeWonNumber(N-1)+ Math.pow(3, N));
+	}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Arrays.fill(arr, 0);
+		
+		System.out.println(getJaeWonNumber(N));
+	}
+}
