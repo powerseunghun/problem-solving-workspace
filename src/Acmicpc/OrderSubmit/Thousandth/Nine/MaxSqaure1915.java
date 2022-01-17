b1c909063a684017e37451082b3925f1bc354a65
anseunghun <size411@naver.com>
2022-01-17 18:31:46 +0900

    el unuse lib

--- src/Acmicpc/OrderSubmit/Thousandth/Nine/MaxSqaure1915.java
+++ src/Acmicpc/OrderSubmit/Thousandth/Nine/MaxSqaure1915.java
@@ -3,7 +3,6 @@ package Acmicpc.OrderSubmit.Thousandth.Nine;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class MaxSqaure1915 {
 	public static void main(String[] args) throws IOException {
