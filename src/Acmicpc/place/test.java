6f05ba6ce1bf8e72a8482cf7491f8ff2d7f0615e
anseunghun <size411@naver.com>
2022-02-12 18:44:25 +0900

    sj commit

--- src/Acmicpc/place/test.java
+++ src/Acmicpc/place/test.java
@@ -3,8 +3,6 @@ package Acmicpc.place;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Pos implements Comparable<Pos>{
