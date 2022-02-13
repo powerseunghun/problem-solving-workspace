075c8215e9f0d4a75ecc5fda24129bd6a1a751f2
anseunghun <size411@naver.com>
2022-02-13 14:28:46 +0900

    simple recursion

---
+++ src/Acmicpc/OrderSubmit/TenThousandth/Eight/One/ThailandYear18108.java
@@ -0,0 +1,14 @@
+package Acmicpc.OrderSubmit.TenThousandth.Eight.One;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ThailandYear18108 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine())-543);
+		
+		br.close();
+	}
+}
