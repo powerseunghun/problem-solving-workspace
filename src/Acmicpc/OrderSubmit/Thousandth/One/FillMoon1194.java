1398fb6a866229a3cf77a7c9b30e2cb18eccb00d
anseunghun <size411@naver.com>
2022-03-11 00:26:56 +0900

    visit check 3dimension & add bitmasking 7f

--- src/Acmicpc/OrderSubmit/Thousandth/One/FillMoon1194.java
+++ src/Acmicpc/OrderSubmit/Thousandth/One/FillMoon1194.java
@@ -3,19 +3,19 @@ package Acmicpc.OrderSubmit.Thousandth.One;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 
 class Loc {
 	private int x;
 	private int y;
 	private int t;
-	public Loc(int x, int y, int t) {
+	private int k;
+	public Loc(int x, int y, int t, int k) {
 		this.x = x;
 		this.y = y;
 		this.t = t;
+		this.k = k;
 	}
 	public int getX() {
 		return x;
@@ -35,55 +35,60 @@ class Loc {
 	public void setT(int t) {
 		this.t = t;
 	}
+	public int getK() {
+		return k;
+	}
+	public void setK(int k) {
+		this.k = k;
+	}
 }
 public class FillMoon1194 {
 	static char[][] arr = null;
-	static boolean[][] check = null;
+	static boolean[][][] check = null;
 	static int startX = 0, startY = 0;
-	static int goalX = 0, goalY = 0;
 	static int[] dirX = {0, -1, 0, 1};
 	static int[] dirY = {-1, 0, 1, 0};
 	static int res = Integer.MAX_VALUE;
-	static Map<Character, Integer> map = new HashMap<>();
 	static void bfs() {
 		Queue<Loc> q = new LinkedList<>();
-		q.add(new Loc(startX, startY, 0));
-		check[startX][startY] = true;
+		q.add(new Loc(startX, startY, 0, 0));
+		check[startX][startY][0] = true;
 		
 		while (!q.isEmpty()) {
 			Loc tmp = q.poll();
-			if (tmp.getX() == goalX && tmp.getY() == goalY) {
+			if (arr[tmp.getX()][tmp.getY()] == '1') {
 				res = Math.min(res, tmp.getT());
-				continue;
 			}
 			
 			for (int i = 0, nx = 0, ny = 0; i < dirX.length; i++) {
 				nx = tmp.getX() + dirX[i];
 				ny = tmp.getY() + dirY[i];
-				if (nx < 0 || ny < 0 || nx > arr.length-1 || ny > arr[nx].length-1) continue;
+				if (nx < 0 || ny < 0 || nx > arr.length-1 || ny > arr[nx].length-1 || arr[nx][ny] == '#') continue;
+				if (check[nx][ny][tmp.getK()]) continue;
+				
+				if (arr[nx][ny] == '.' || arr[nx][ny] == '0' || arr[nx][ny] == '1') {
+					check[nx][ny][tmp.getK()] = true;
+					q.add(new Loc(nx, ny, tmp.getT()+1, tmp.getK()));
+				}
 				
 				// key
-				if (arr[nx][ny] >= 'a' && arr[nx][ny] <= 'z') {
-					if (!map.containsKey(arr[nx][ny])) {
-						map.put(arr[nx][ny], 1);
+				else if (arr[nx][ny] >= 'a' && arr[nx][ny] <= 'z') {
+					int key = 1 << (arr[nx][ny] - 'a');
+					key = tmp.getK() | key;
+					if (!check[nx][ny][key]) {
+						check[nx][ny][tmp.getK()] = true;
+						check[nx][ny][key] = true;
+						q.add(new Loc(nx, ny, tmp.getT()+1, key));
 					}
-					else map.put(arr[nx][ny], map.get(arr[nx][ny])+1);
-					q.add(new Loc(nx, ny, tmp.getT()+1));
 				}
+				
 				// door
-				else if (arr[nx][ny] == 'A' && arr[nx][ny] <= 'Z') {
-					char key = (char)(arr[nx][ny] + 32);
-					if (map.containsKey(key)) {
-						int leftKey = map.get(key)-1;
-						if (leftKey == 0) {
-							map.remove(key);
-						}
-						else map.put(key, leftKey);
-						q.add(new Loc(nx, ny, tmp.getT()+1));
+				else if (arr[nx][ny] >= 'A' && arr[nx][ny] <= 'Z') {
+					int door = 1 << (arr[nx][ny] - 'A');
+					if ((door & tmp.getK()) > 0) {
+						check[nx][ny][tmp.getK()] = true;
+						q.add(new Loc(nx, ny, tmp.getT()+1, tmp.getK()));
 					}
-				}
-				else if (arr[nx][ny] == '.') {
-					q.add(new Loc(nx, ny, tmp.getT()+1));
 				}
 			}
 		}
@@ -94,7 +99,7 @@ public class FillMoon1194 {
 		int N = Integer.parseInt(tmp.split(" ")[0]);
 		int M = Integer.parseInt(tmp.split(" ")[1]);
 		arr = new char[N][M];
-		check = new boolean[N][M];
+		check = new boolean[N][M][64];
 		
 		for (int i = 0; i < arr.length; i++) {
 			tmp = br.readLine();
@@ -103,10 +108,6 @@ public class FillMoon1194 {
 				if (arr[i][j] == '0') {
 					startX = i;
 					startY = j;
-				}
-				else if (arr[i][j] == '1') {
-					goalX = i;
-					goalY = j;
 				}
 			}
 		}
