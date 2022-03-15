b80e9a6f33fe8512504cef07a5a750e56b688253
anseunghun <size411@naver.com>
2022-03-16 02:51:54 +0900

    PinMoney BinarySearch

--- src/Acmicpc/OrderSubmit/Thousandth/Six/Two/PinMoneyManagement6236.java
+++ src/Acmicpc/OrderSubmit/Thousandth/Six/Two/PinMoneyManagement6236.java
@@ -6,44 +6,18 @@ import java.io.InputStreamReader;
 
 public class PinMoneyManagement6236 {
 	static int[] arr = null;
-	static boolean check(int m, int limit) {
-		int cnt = 0, tmpMoney = 0;
-		for (int i = 0; i < arr.length; i++) {
-			if (m < arr[i]) return true;
-			if (tmpMoney < arr[i]) {
-				tmpMoney = m;
-				cnt++;
-				if (cnt > limit) {
-					return true;
-				}
-			}
-			tmpMoney -= arr[i];
-		}
-		return false;
-	}
 	static int binarySearch(int l, int r, int M) {
 		while (l <= r) {
-			int m = (l+r) / 2;// count = 0, tmpMoney = 0;
-//			boolean flag = false;
-//			for (int i = 0; i < arr.length; i++) {
-//				if (m < arr[i]) {
-//					l = m+1;
-//					flag = true;
-//					break;
-//				}
-//				if (tmpMoney < arr[i]) {
-//					tmpMoney = m;
-//					count++;
-//					if (count > M) {
-//						l = m+1;
-//						flag = true;
-//						break;
-//					}
-//				}
-//				tmpMoney -= arr[i];
-//			}
-//			if (!flag) r = m-1;
-			if (check(m, M)) l = m+1;
+			int m = (l+r) / 2;
+			int cnt = 1, tmpMoney = m;
+			for (int i = 0; i < arr.length; i++) {
+				if (tmpMoney < arr[i]) {
+					tmpMoney = m;
+					cnt++;
+				}
+				tmpMoney -= arr[i];
+			}
+			if (cnt > M) l = m+1;
 			else r = m-1;
 		}
 		return l;
@@ -52,15 +26,16 @@ public class PinMoneyManagement6236 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String tmp = br.readLine();
 		int N = Integer.parseInt(tmp.split(" ")[0]), M = Integer.parseInt(tmp.split(" ")[1]);
-		int max = Integer.MIN_VALUE;
+		int max = 0, sum = 0;
 		arr = new int[N];
 		
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
 			max = Math.max(max, arr[i]);
 		}
 		
-		System.out.println(binarySearch(1, max*M, max));
+		System.out.println(binarySearch(max, sum, M));
 		br.close();
 	}
 }
