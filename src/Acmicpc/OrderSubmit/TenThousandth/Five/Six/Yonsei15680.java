b47e4f32d89f7ed886239119ae49c7146bd495a9
anseunghun <size411@naver.com>
2022-03-28 22:14:52 +0900

    Yonesei simple

---
+++ src/Acmicpc/OrderSubmit/TenThousandth/Five/Six/Yonsei15680.java
@@ -0,0 +1,11 @@
+package Acmicpc.OrderSubmit.TenThousandth.Five.Six;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Yonsei15680 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println(Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine()) == 0 ? "YONSEI" : "Leading the Way to the Future");
+	}
+}
