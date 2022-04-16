d8e04f93645ac00946dc405a4b3a49dc1a76e41b
anseunghun <size411@naver.com>
2022-04-16 19:26:27 +0900

    simple impl

---
+++ src/Codeforce/OrderSubmit/TwoHundred/NearlyLuckNumber.java
@@ -0,0 +1,18 @@
+package Codeforce.OrderSubmit.TwoHundred;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class NearlyLuckNumber {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int count = 0;
+		
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '4' || str.charAt(i) == '7') count++;
+		}
+		System.out.println(count == 4 || count == 7 ? "YES" : "NO");
+		br.close();
+	}
+}
