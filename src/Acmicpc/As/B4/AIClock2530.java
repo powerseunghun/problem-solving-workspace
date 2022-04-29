1e1b50d5335c115d62416b1e3c262781e26b6e10
anseunghun <size411@naver.com>
2022-04-29 18:49:15 +0900

    B4 as simple impl

---
+++ src/Acmicpc/As/B4/AIClock2530.java
@@ -0,0 +1,34 @@
+package Acmicpc.As.B4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AIClock2530 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String tmp = br.readLine();
+		int h = Integer.parseInt(tmp.split(" ")[0]);
+		int m = Integer.parseInt(tmp.split(" ")[1]);
+		int s = Integer.parseInt(tmp.split(" ")[2]);
+		int d = Integer.parseInt(br.readLine());
+		
+		s += d % 60;
+		if (s >= 60) {
+			m += (s / 60);
+			s %= 60;
+		}
+		
+		m += d / 60;
+		if (m >= 60) {
+			h += (m / 60);
+			m %= 60;
+		}
+		if (h >= 24) {
+			h %= 24;
+		}
+		
+		System.out.println(h + " " + m + " " + s);
+		br.close();
+	}
+}
---
+++ src/Acmicpc/As/B4/TimeCard5575.java
@@ -0,0 +1,35 @@
+package Acmicpc.As.B4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class TimeCard5575 {
+	static int[] arr = new int[6];
+	static void calc() {
+		for (int i = 0, idx = 5; i < 3; i++, idx--) {
+			arr[idx] -= arr[idx-3];
+			if (arr[idx] < 0) {
+				arr[idx-1] -= 1;
+				arr[idx] += 60;
+			}
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		for (int i = 0; i < 3; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < arr.length; j++) {
+				arr[j] = Integer.parseInt(st.nextToken());
+			}
+			calc();
+			sb.append(arr[3] + " " + arr[4] + " " + arr[5] + "\n");
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
