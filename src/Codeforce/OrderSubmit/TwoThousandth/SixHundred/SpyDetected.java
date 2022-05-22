aef829bfb1e6a5f8645571a2ebf96f337f96c317
anseunghun <size411@naver.com>
2022-05-22 16:00:05 +0900

    cf simple

--- src/Codeforce/OrderSubmit/TwoThousandth/SixHundred/SpyDetected.java
+++ src/Codeforce/OrderSubmit/TwoThousandth/SixHundred/SpyDetected.java
@@ -7,30 +7,29 @@ import java.util.StringTokenizer;
 
 public class SpyDetected {
 	static int[] arr = null;
+	static int[] count = null;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
 		int t = Integer.parseInt(br.readLine()), n = 0;
-		boolean flag = true;
 		
 		while (t-- > 0) {
 			n = Integer.parseInt(br.readLine());
-			flag = false;
-			arr = new int[n+1];
+			count = new int[101];
+			arr = new int[n];
 			st = new StringTokenizer(br.readLine());
 			
-			for (int i = 1; i < arr.length; i++) {
+			for (int i = 0; i < arr.length; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
-				if (i == 1) continue;
-				
-				if (arr[i-1] != arr[i]) {
-					flag = true;
-					sb.append(i+"\n");
+				count[arr[i]]++;
+			}
+			for (int i = 0; i < arr.length; i++) {
+				if (count[arr[i]] == 1) {
+					sb.append((i+1)+"\n");
 					break;
 				}
 			}
-			if (!flag) sb.append("1\n");
 		}
 		
 		System.out.print(sb.toString());
