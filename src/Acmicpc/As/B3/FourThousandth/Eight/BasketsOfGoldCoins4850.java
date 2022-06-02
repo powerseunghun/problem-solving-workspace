87d4584d349b31d4e41bf6deb4ecd53346944673
anseunghun <size411@naver.com>
2022-06-02 20:11:43 +0900

    add anotation

--- src/Acmicpc/As/B3/FourThousandth/Eight/BasketsOfGoldCoins4850.java
+++ src/Acmicpc/As/B3/FourThousandth/Eight/BasketsOfGoldCoins4850.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// as yet
 public class BasketsOfGoldCoins4850 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
