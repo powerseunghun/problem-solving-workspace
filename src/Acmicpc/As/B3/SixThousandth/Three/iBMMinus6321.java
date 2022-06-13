dc478c1993010d1afb4b5e54ab9646531ae82c7d
anseunghun <size411@naver.com>
2022-06-13 22:36:03 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/SixThousandth/Three/iBMMinus6321.java
@@ -0,0 +1,35 @@
+package Acmicpc.As.B3.SixThousandth.Three;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class iBMMinus6321 {
+	static Map<Character, Character> map = new HashMap<>();
+	static void mapInit() {
+		for (char c = 'A'; c <= 'Z'; c++) {
+			map.put(c, (char)((c+1) >= 91 ? (c+1-26) : (c+1)));  
+		}
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String tmp = null;
+		int T = Integer.parseInt(br.readLine()), idx = 1;
+		
+		mapInit();
+		while (T-- > 0) {
+			tmp = br.readLine();
+			sb.append("String #" + (idx++) + "\n");
+			for (int i = 0; i < tmp.length(); i++) {
+				sb.append(map.get(tmp.charAt(i)));
+			}
+			sb.append("\n\n");
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
