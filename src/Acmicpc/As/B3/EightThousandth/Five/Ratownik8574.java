8cbb577b0c74514435e3da426402ad47e972c7a9
powerseunghun <size411@naver.com>
2022-06-22 17:29:28 +0900

    b3 as

--- src/Acmicpc/As/B3/EightThousandth/Five/Ratownik8574.java
+++ src/Acmicpc/As/B3/EightThousandth/Five/Ratownik8574.java
@@ -5,27 +5,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Ratownik8574 {
-	static boolean check(int cx, int cy, int k, int x, int y) {
-		if (((x-k) <= cx && cx <= (x+k)) && ((y-k) <= cy && cy <= (y+k))) return true;
-		return false;
+	static boolean check(double cx, double cy, double k, double x, double y) {
+		if (Math.sqrt(Math.pow(cx-x, 2) + Math.pow(cy-y, 2)) > k) return false;
+		return true;
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String tmp = br.readLine();
-		int n = Integer.parseInt(tmp.split(" ")[0]);
-		int k = Integer.parseInt(tmp.split(" ")[1]);
-		int x = Integer.parseInt(tmp.split(" ")[2]);
-		int y = Integer.parseInt(tmp.split(" ")[3]), res = 0;
+		double n = Double.parseDouble(tmp.split(" ")[0]);
+		double k = Double.parseDouble(tmp.split(" ")[1]);
+		double x = Double.parseDouble(tmp.split(" ")[2]);
+		double y = Double.parseDouble(tmp.split(" ")[3]);
+		double cx = 0, cy = 0;
+		int res = 0;
 		
-		for (int i = 0, cx = 0, cy = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			tmp = br.readLine();
-			cx = Integer.parseInt(tmp.split(" ")[0]);
-			cy = Integer.parseInt(tmp.split(" ")[1]);
+			cx = Double.parseDouble(tmp.split(" ")[0]);
+			cy = Double.parseDouble(tmp.split(" ")[1]);
 			
-			if (check(cx, cy, k, x, y)) res++;
+			if (!check(cx, cy, k, x, y)) res++;
 		}
 		
-		System.out.println(n-res);
+		System.out.println(res);
 		br.close();
 	}
 }
