8cb654459e716c968ab6844df56f9d8163663fc8
anseunghun <size411@naver.com>
2022-06-24 21:48:18 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/EightThousandth/Seven/Schronisko8760.java
@@ -0,0 +1,23 @@
+package Acmicpc.As.B3.EightThousandth.Seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Schronisko8760 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		StringBuilder sb = new StringBuilder();
+		Scanner sc = new Scanner(System.in);
+		int z = sc.nextInt(), w = 0, k = 0;
+		
+		for (int i = 0; i < z; i++) {
+			w = sc.nextInt();
+			k = sc.nextInt();
+			
+			sb.append((w*k/2) + "\n");
+		}
+		
+		System.out.print(sb.toString());
+	}
+}
