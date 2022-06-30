e4f1ffa3f4d2e1c2d4286729b956460b2deebc1f
anseunghun <size411@naver.com>
2022-06-30 14:36:24 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/NineThousandth/VonNeumann9469.java
@@ -0,0 +1,29 @@
+package Acmicpc.As.B3.NineThousandth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class VonNeumann9469 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int P = Integer.parseInt(br.readLine());
+		double D = 0, A = 0, B = 0, F = 0, res = 0;
+		String tmp = null;
+		
+		while (P-- > 0) {
+			tmp = br.readLine();
+			sb.append(Integer.parseInt(tmp.split(" ")[0]) + " ");
+			D = Double.parseDouble(tmp.split(" ")[1]);
+			A = Double.parseDouble(tmp.split(" ")[2]);
+			B = Double.parseDouble(tmp.split(" ")[3]);
+			F = Double.parseDouble(tmp.split(" ")[4]);
+			res = D / (A+B) * F;
+			sb.append(String.format("%.6f\n", res));
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
