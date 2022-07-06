c1e1d5e5ff8348b93690802ae193d88a8057803f
powerseunghun <size411@naver.com>
2022-07-06 13:39:37 +0900

    b3 as

--- src/Acmicpc/As/B3/NineThousandth/Six/CabinBaggage9622.java
+++ src/Acmicpc/As/B3/NineThousandth/Six/CabinBaggage9622.java
@@ -1,8 +1,6 @@
 package Acmicpc.As.B3.NineThousandth.Six;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class CabinBaggage9622 {
