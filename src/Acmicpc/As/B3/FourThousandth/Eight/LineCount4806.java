32282cc3e9f70dd39375d77a1e0eb8d86e1cc70d
powerseunghun <size411@naver.com>
2022-07-07 09:49:06 +0900

    b3 asg

--- src/Acmicpc/As/B3/FourThousandth/Eight/LineCount4806.java
+++ src/Acmicpc/As/B3/FourThousandth/Eight/LineCount4806.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import Acmicpc.Sorting.TwoThousandth.Nine.FeequencySort2910;
-
 public class LineCount4806 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
