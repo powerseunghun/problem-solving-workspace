aeba8c1adc225fadcc3e7b67d21a10bccf3b42d1
anseunghun <size411@naver.com>
2022-07-18 09:36:58 +0900

    b4 as append

---
+++ src/Acmicpc/As/B4/TenThousandth/Five/Zero/CAPS15000.java
@@ -0,0 +1,14 @@
+package Acmicpc.As.B4.TenThousandth.Five.Zero;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CAPS15000 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().toUpperCase());
+		
+		br.close();
+	}
+}
