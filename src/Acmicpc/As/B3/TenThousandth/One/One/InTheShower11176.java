aa9538ec1b2680f25c3cb00ac8368449888a742a
powerseunghun <size411@naver.com>
2022-07-19 09:54:04 +0900

    b3 as

--- src/Acmicpc/As/B3/TenThousandth/One/One/InTheShower11176.java
+++ src/Acmicpc/As/B3/TenThousandth/One/One/InTheShower11176.java
@@ -4,25 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class NegativePeopleInDaHouse11161 {
-	// 4f
+public class InTheShower11176 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine()), M = 0, P1 = 0, P2 = 0, total = 0, w = 0;
 		String tmp = null;
+		int T = Integer.parseInt(br.readLine()), E = 0, N = 0, cnt = 0;
 		
 		while (T-- > 0) {
-			M = Integer.parseInt(br.readLine());
-			total = 0;
-			for (int i = 0; i < M; i++) {
-				tmp = br.readLine();
-				P1 = Integer.parseInt(tmp.split(" ")[0]);
-				P2 = Integer.parseInt(tmp.split(" ")[1]);
-				if (P2 > P1) total += P2-(P1+w);
-				else if (P1 > P2) w += P1;
+			tmp = br.readLine();
+			E = Integer.parseInt(tmp.split(" ")[0]);
+			N = Integer.parseInt(tmp.split(" ")[1]);
+			cnt = 0;
+			for (int i = 0; i < N; i++) {
+				if (Integer.parseInt(br.readLine()) > E) cnt++;
 			}
-			sb.append(total + "\n");
+			sb.append(cnt + "\n");
 		}
 		
 		System.out.print(sb.toString());
