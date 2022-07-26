922da182e30ab86fb0cbcbdbfaa4d0eddbf72ada
powerseunghun <size411@naver.com>
2022-07-26 15:22:04 +0900

    b3 as

--- src/Acmicpc/As/B3/TenThousandth/One/Seven/POT11772.java
+++ src/Acmicpc/As/B3/TenThousandth/One/Seven/POT11772.java
@@ -4,12 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BottledUpFeelings11759 {
+public class POT11772 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String tmp = br.readLine();
-		int s = Integer.parseInt(tmp.split(" ")[0]);
-		int v1 = Integer.parseInt(tmp.split(" ")[1]);
-		int v1 = Integer.parseInt(tmp.split(" ")[2]);
+		int N = Integer.parseInt(br.readLine()), X = 0;
+		
+		for (int i = 0, val = 0; i < N; i++) {
+			val = Integer.parseInt(br.readLine());
+			X += Math.pow(val/10, val%10);
+		}
+		
+		System.out.println(X);
+		br.close();
 	}
 }
