84a17e840a4cae3f42f5e244e0f156b90c41be37
powerseunghun <size411@naver.com>
2022-07-26 15:06:07 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/TenThousandth/One/Seven/BottledUpFeelings11759.java
@@ -0,0 +1,15 @@
+package Acmicpc.As.B3.TenThousandth.One.Seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BottledUpFeelings11759 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String tmp = br.readLine();
+		int s = Integer.parseInt(tmp.split(" ")[0]);
+		int v1 = Integer.parseInt(tmp.split(" ")[1]);
+		int v1 = Integer.parseInt(tmp.split(" ")[2]);
+	}
+}
