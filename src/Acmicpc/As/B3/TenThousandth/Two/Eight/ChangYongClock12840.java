b8dd08ecd62076579f94c2bd2acbc2db5a4b7ec5
powerseunghun <size411@naver.com>
2022-08-02 10:54:39 +0900

    b3 as

--- src/Acmicpc/As/B3/TenThousandth/Two/Eight/ChangYongClock12840.java
+++ src/Acmicpc/As/B3/TenThousandth/Two/Eight/ChangYongClock12840.java
@@ -5,52 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ChangYongClock12840 {
-	static int[] arr = new int[3];
-	static void calc(int T, int c) {
-		arr[2] += T == 1 ? c : c*-1;
-		if (T == 1) {
-			for (int i = 1; i >= 0; i--) {
-				arr[i] += (arr[i+1]/60);
-				arr[i+1] %= 60;
-			}
-			arr[0] %= 24;
-		}
-		else {
-			for (int i = 2, n = 0; i >= 1; i--) {
-				if (arr[i] >= 0) continue;
-				n = Math.abs(arr[i]);
-				n = n%60 == 0 ? n/60 : n/60+1;
-				arr[i] += 60*n;
-				arr[i-1] -= n;
-			}
-			arr[0] = arr[0] < 0 ? 24-(Math.abs(arr[0])%24) : arr[0];
-		}
-	}
-	static void print() {
-		for (int el : arr) {
-			System.out.print(el + " ");
-		}
-		System.out.println();
-	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String tmp = br.readLine();
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(tmp.split(" ")[i]);
-		}
-		int q = Integer.parseInt(br.readLine()), T = 0, c = 0;
+		int h = Integer.parseInt(tmp.split(" ")[0]);
+		int m = Integer.parseInt(tmp.split(" ")[1]);
+		int s = Integer.parseInt(tmp.split(" ")[2]);
+		int q = Integer.parseInt(br.readLine()), T = 0, c = 0, t = 0;
 		
+		t = h*3600 + m*60 + s;
 		while (q-- > 0) {
 			tmp = br.readLine();
 			T = Integer.parseInt(tmp.split(" ")[0]);
-			
 			switch(T) {
 			case 1: case 2:
 				c = Integer.parseInt(tmp.split(" ")[1]);
-				calc(T, c);
+				t += T == 1 ? c : (c*-1);
+				while (t < 0) t+=86400;
+				t = t%86400;
 				break;
 			case 3:
-				print();
+				System.out.println((t/3600) + " " + (t/60)%60 + " " + t%60);
 				break;
 			}
 		}
