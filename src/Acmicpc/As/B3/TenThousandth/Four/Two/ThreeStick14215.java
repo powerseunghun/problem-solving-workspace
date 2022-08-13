5033fa6bdfb3b39f56647dcb5430e0003fe5b243
anseunghun <size411@naver.com>
2022-08-13 23:35:47 +0900

    b3 as

--- src/Acmicpc/As/B3/TenThousandth/Four/Two/ThreeStick14215.java
+++ src/Acmicpc/As/B3/TenThousandth/Four/Two/ThreeStick14215.java
@@ -1,5 +1,23 @@
 package Acmicpc.As.B3.TenThousandth.Four.Two;
 
-public class ThreeStick14215 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class ThreeStick14215 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[3];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0]+arr[1]+Math.min(arr[0]+arr[1]-1, arr[2]));
+		br.close();
+	}
 }
