56f470151f084b89c59056460dbe504450d0b0de
anseunghun <size411@naver.com>
2022-08-15 23:24:46 +0900

    b3 as bruteforce

---
+++ src/Acmicpc/As/B3/TenThousandth/Four/Five/YProgramming14568.java
@@ -0,0 +1,27 @@
+package Acmicpc.As.B3.TenThousandth.Four.Five;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class YProgramming14568 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+		
+		// 택희,영훈, 남규
+		for (int i = 1; i <= N; i++) {
+			for (int j = N-i, k = 0; j >= 1; j--) {
+				if (i % 2 != 0) continue;
+				k = N - (i + j);
+				if (k == 0) continue;
+				if (k-2 < j) continue;
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+		br.close();
+	}
+}
