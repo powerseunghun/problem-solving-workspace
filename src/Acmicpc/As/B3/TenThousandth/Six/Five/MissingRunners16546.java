2e38f51bdf40c4142696210095c51273e08acc1b
anseunghun <size411@naver.com>
2022-09-03 17:00:01 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/TenThousandth/Six/Five/MissingRunners16546.java
@@ -0,0 +1,32 @@
+package Acmicpc.As.B3.TenThousandth.Six.Five;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class MissingRunners16546 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()), val = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		while (st.hasMoreTokens()) {
+			val = Integer.parseInt(st.nextToken());
+			map.put(val, map.getOrDefault(val, 0)+1);
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			if (!map.containsKey(i)) {
+				val = i;
+				break;
+			}
+		}
+		
+		System.out.println(val);
+		br.close();
+	}
+}
