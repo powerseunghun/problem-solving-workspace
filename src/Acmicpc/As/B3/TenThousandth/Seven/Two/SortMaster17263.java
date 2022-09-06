77914d9dbe92c63f2b30b7fcf3aa98a727b0ff39
anseunghun <size411@naver.com>
2022-09-06 19:05:35 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/TenThousandth/Seven/Two/SortMaster17263.java
@@ -0,0 +1,25 @@
+package Acmicpc.As.B3.TenThousandth.Seven.Two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class SortMaster17263 {
+	static int[] arr = null;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		System.out.println(arr[arr.length-1]);
+		br.close();
+	}
+}
