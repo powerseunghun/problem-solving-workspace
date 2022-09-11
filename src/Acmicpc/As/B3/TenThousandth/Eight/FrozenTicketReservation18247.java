bc06ad11129baa4403beb097ce827c1fa225c163
anseunghun <size411@naver.com>
2022-09-11 16:01:11 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/TenThousandth/Eight/FrozenTicketReservation18247.java
@@ -0,0 +1,28 @@
+package Acmicpc.As.B3.TenThousandth.Eight;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FrozenTicketReservation18247 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine()), N = 0, M = 0;
+		String tmp = null;
+		
+		while (T-- > 0) {
+			tmp = br.readLine();
+			N = Integer.parseInt(tmp.split(" ")[0]);
+			M = Integer.parseInt(tmp.split(" ")[1]);
+			if (N < 12 || M < 4) {
+				sb.append("-1\n");
+				continue;
+			}
+			sb.append((11*M+4) + "\n");
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
--- src/Acmicpc/As/B3/TenThousandth/Eight/One/ShapeCrystal18127.java
+++ src/Acmicpc/As/B3/TenThousandth/Eight/One/ShapeCrystal18127.java
@@ -10,10 +10,6 @@ public class ShapeCrystal18127 {
 		String tmp = br.readLine();
 		int tmpA = Integer.parseInt(tmp.split(" ")[0]);
 		int tmpB = Integer.parseInt(tmp.split(" ")[1]);
-		// 3 = 2,3,4,5
-		// 4 = 3,5,7,9
-		// 5 = 4,7,10,13
-		// a = (a-1) / d = (a-2)
 		long a = tmpA-1, d = tmpA-2, res = 0;
 		
 		res = tmpB * (2*a + (tmpB-1) * d) / 2;
