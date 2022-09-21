c4ff8ac84159ac6b818ad56300a963e07a123370
anseunghun <size411@naver.com>
2022-09-22 02:00:47 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/TwentyThousandth/One/Five/MakePassword21553.java
@@ -0,0 +1,15 @@
+package Acmicpc.As.B3.TwentyThousandth.One.Five;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MakePassword21553 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		
+		System.out.println(br.readLine());
+		br.close();
+	}
+}
