ee03725962dc58cb52ee13b4ad28ebda12e47842
anseunghun <size411@naver.com>
2022-09-25 15:55:52 +0900

    b4 as append

---
+++ src/Acmicpc/As/B4/TwentyThousandth/Four/Two/AlgorithmRunningTime24263.java
@@ -0,0 +1,16 @@
+package Acmicpc.As.B4.TwentyThousandth.Four.Two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AlgorithmRunningTime24263 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println(n);
+		System.out.println("1");
+		br.close();
+	}
+}
