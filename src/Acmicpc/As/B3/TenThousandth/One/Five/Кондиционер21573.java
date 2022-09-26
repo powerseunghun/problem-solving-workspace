f21c9cc7cce8193da3affc84f378d3dfe8a94957
anseunghun <size411@naver.com>
2022-09-26 10:39:54 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/TenThousandth/One/Five/Кондиционер21573.java
@@ -0,0 +1,31 @@
+package Acmicpc.As.B3.TenThousandth.One.Five;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Кондиционер21573 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String tmp = br.readLine();
+		int c = Integer.parseInt(tmp.split(" ")[0]);
+		int w = Integer.parseInt(tmp.split(" ")[1]);
+		int res = 0;
+		tmp = br.readLine();
+		
+		switch(tmp) {
+		case "heat":
+			res = w>c ? w : c;
+			break;
+		case "freeze":
+			res = c > w ? w : c;
+			break;
+		default:
+			res = tmp.equals("auto") ? w : c;
+			break;
+		}
+		
+		System.out.println(res);
+		br.close();
+	}
+}
