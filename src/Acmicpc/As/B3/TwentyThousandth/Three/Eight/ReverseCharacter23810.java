03ac5162e416e6e3c35810da29d2ab514cae294c
powerseunghun <size411@naver.com>
2022-10-16 00:28:03 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/TwentyThousandth/Three/Eight/ReverseCharacter23810.java
@@ -0,0 +1,41 @@
+package Acmicpc.As.B3.TwentyThousandth.Three.Eight;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReverseCharacter23810 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N*5; j++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N*5; j++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+		for (int i = 0; i < N*2; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
