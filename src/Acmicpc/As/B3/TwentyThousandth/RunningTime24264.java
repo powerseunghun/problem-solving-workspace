e6d3a81455f4ddfe3302b4fe6a2e4445025edbe9
powerseunghun <size411@naver.com>
2022-10-18 18:52:29 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/TwentyThousandth/RunningTime24264.java
@@ -0,0 +1,16 @@
+package Acmicpc.As.B3.TwentyThousandth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RunningTime24264 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Integer.parseInt(br.readLine());
+		
+		System.out.println(N*N);
+		System.out.println(2);
+		br.close();
+	}
+}
