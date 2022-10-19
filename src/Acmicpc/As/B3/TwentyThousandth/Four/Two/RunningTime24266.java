bca5addcc301e249718674cfa3215990a9566ffb
anseunghun <size411@naver.com>
2022-10-20 01:23:26 +0900

    b3 as

---
+++ src/Acmicpc/As/B3/TwentyThousandth/Four/Two/RunningTime24266.java
@@ -0,0 +1,16 @@
+package Acmicpc.As.B3.TwentyThousandth.Four.Two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RunningTime24266 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Integer.parseInt(br.readLine());
+		
+		System.out.println(N*N*N);
+		System.out.println(3);
+		br.close();
+	}
+}
