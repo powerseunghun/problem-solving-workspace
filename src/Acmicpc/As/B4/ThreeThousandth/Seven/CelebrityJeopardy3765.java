dee3f9b211d28364a7fc3008a001aaee5f7265e1
anseunghun <size411@naver.com>
2022-11-07 22:38:21 +0900

    b4 as append

---
+++ src/Acmicpc/As/B4/ThreeThousandth/Seven/CelebrityJeopardy3765.java
@@ -0,0 +1,18 @@
+package Acmicpc.As.B4.ThreeThousandth.Seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CelebrityJeopardy3765 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String tmp = null;
+		
+		while ((tmp = br.readLine()) != null) {
+			System.out.println(tmp);
+		}
+		
+		br.close();
+	}
+}
