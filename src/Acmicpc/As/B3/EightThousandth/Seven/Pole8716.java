a4cf25dda9e61442d54691c7727c933d0dc15a6b
anseunghun <size411@naver.com>
2022-11-19 20:47:09 +0900

    b3 as

--- src/Acmicpc/As/B3/EightThousandth/Seven/Pole8716.java
+++ src/Acmicpc/As/B3/EightThousandth/Seven/Pole8716.java
@@ -7,16 +7,6 @@ import java.io.InputStreamReader;
 public class Pole8716 {
 	static int[] arr = new int[8];
 	static boolean check() {
-		// r1      r2
-		// 0 1 2 3 4 5 6 7
-		// r1 x1 = 0
-		// r1 y1 = 1
-		// r1 x2 = 2
-		// r1 y2 = 3
-		// r2 x1 = 4
-		// r2 y1 = 5
-		// r2 x2 = 6
-		// r2 y2 = 7
 		if (arr[2] < arr[4] || arr[6] < arr[0] || arr[7] > arr[1] || arr[3] > arr[5]) return true;
 		return false;
 	}
