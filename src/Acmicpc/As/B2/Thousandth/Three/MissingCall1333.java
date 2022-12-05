3a540e4ec592b5f0ab9bedd5c337d6367dbc1ec2
anseunghun <size411@naver.com>
2022-12-05 22:11:17 +0900

    b2 as

--- src/Acmicpc/As/B2/Thousandth/Three/MissingCall1333.java
+++ src/Acmicpc/As/B2/Thousandth/Three/MissingCall1333.java
@@ -3,7 +3,6 @@ package Acmicpc.As.B2.Thousandth.Three;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class MissingCall1333 {
 	static boolean[] arr = null;
