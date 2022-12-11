e672d11741af253b42c5b6bbcb2e28324bb59c78
anseunghun <size411@naver.com>
2022-12-11 14:44:34 +0900

    b2 as

--- src/Acmicpc/As/B2/Thousandth/Six/DNA1672.java
+++ src/Acmicpc/As/B2/Thousandth/Six/DNA1672.java
@@ -3,43 +3,44 @@ package Acmicpc.As.B2.Thousandth.Six;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DNA1672 {
-	static String func(String str) {
-		char c1 = str.charAt(str.length()-1);
-		char c2 = str.charAt(str.length()-2);
-		char c3 = ' ';
-		if (c1 == c2) {
-			c3 = c1;
+	static Map<Character, Integer> map = new HashMap<>();
+	static char[][] dna = {
+			{'A', 'C', 'A', 'G'},
+			{'C', 'G', 'T', 'A'},
+			{'A', 'T', 'C', 'G'},
+			{'G', 'A', 'G', 'T'}
+	};
+	static void mapInit() {
+		char[] tArr = {'A', 'G', 'C', 'T'};
+		for (int i = 0; i < tArr.length; i++) {
+			map.put(tArr[i], i);
 		}
-		else {
-			switch(c1) {
-			case 'A':
-				c3 = c2 == 'G' ? 'C': c2 == 'C' ? 'A' : 'G';
-				break;
-			case 'G':
-				c3 = c2 == 'A' ? 'C': c2 == 'C' ? 'T' : 'A';
-				break;
-			case 'C':
-				c3 = c2 == 'A' ? 'A': c2 == 'G' ? 'T' : 'G';
-				break;
-			case 'T':
-				c3 = c2 == 'A' ? 'G': c2 == 'G' ? 'A' : 'G';
-				break;
-			}
-		}
-		return (str.substring(0, str.length()-2) + c3);
+	}
+	static char func(char c1, char c2) {
+		return dna[map.get(c1)][map.get(c2)];
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		br.readLine();
+		int N = Integer.parseInt(br.readLine());
 		String str = br.readLine();
+		char res = ' ';
+		mapInit();
 		
-		while(str.length() != 1) {
-			str = func(str);
+		if (N == 1) {
+			res = str.charAt(0);
+		}
+		else {
+			res = func(str.charAt(str.length()-1), str.charAt(str.length()-2));
+			for (int i = str.length()-3; i >= 0; i--) {
+				res = func(res, str.charAt(i));
+			}
 		}
 		
-		System.out.println(str);
+		System.out.println(res);
 		br.close();
 	}
 }
