6acb7a1748b2a764703288240915394053e414cf
anseunghun <size411@naver.com>
2022-12-17 16:09:09 +0900

    s3 as chg java to c++

--- src/Acmicpc/As/S3/TenThousandth/Two/Seven/Product12779.java
+++ src/Acmicpc/As/S3/TenThousandth/Two/Seven/Product12779.java
@@ -3,31 +3,27 @@ package Acmicpc.As.S3.TenThousandth.Two.Seven;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class Product12779 {
-	static BigInteger GCD(BigInteger A, BigInteger B) {
-		if (B.equals(BigInteger.valueOf(0))) return A;
-		else return GCD(B, A.mod(B));
+	static long GCD(long A, long B) {
+		if (B == 0) return A;
+		else return GCD(B, A%B);
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String tmp = br.readLine();
-		BigInteger A = new BigInteger(tmp.split(" ")[0]);
-		BigInteger B = new BigInteger(tmp.split(" ")[1]);
-		BigInteger C = new BigInteger("0");
-		BigInteger D = null;
+		long A = Long.parseLong(tmp.split(" ")[0]);
+		long B = Long.parseLong(tmp.split(" ")[1]), D = 0;
+		int C = 0;
+
+		C = (int)(Math.sqrt(B)) - (int)(Math.sqrt(A));
 		
-		for (long i = (long)(Math.sqrt(A.longValue())); i*i <= B.longValue(); i++) {
-			C = i*i > A.longValue() ? C.add(new BigInteger("1")) : C.add(new BigInteger("0"));
-		}
-		
-		if (C.equals(BigInteger.valueOf(0))) {
+		if (C == 0) {
 			System.out.println("0");
 		}
 		else {
-			D = GCD(B.subtract(A), C);
-			System.out.println(C.divide(D) + "/" + (B.subtract(A).divide(D)));
+			D = GCD(C, B-A);
+			System.out.println(C/D + "/" + (B-A)/D);
 		}
 	}
 }
