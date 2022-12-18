98685a28cb00046c6d1c2d90f4daa5a0034a5818
anseunghun <size411@naver.com>
2022-12-18 17:22:09 +0900

    b5 as b2 as mod

---
+++ src/Acmicpc/As/B5/TenThousandth/Zero/One/Hook10189.java
@@ -0,0 +1,10 @@
+package Acmicpc.As.B5.TenThousandth.Zero.One;
+
+public class Hook10189 {
+	public static void main(String[] args) {
+		System.out.println("#  # #### #### #  #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#  # #### #### #  #");
+	}
+}
--- src/Acmicpc/As/S3/TenThousandth/Two/Seven/Product12779.java
+++ src/Acmicpc/As/S3/TenThousandth/Two/Seven/Product12779.java
@@ -6,24 +6,26 @@ import java.io.InputStreamReader;
 
 public class Product12779 {
 	static long GCD(long A, long B) {
-		if (B == 0) return A;
+		if (B == 0L) return A;
 		else return GCD(B, A%B);
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String tmp = br.readLine();
 		long A = Long.parseLong(tmp.split(" ")[0]);
-		long B = Long.parseLong(tmp.split(" ")[1]), D = 0;
-		int C = 0;
+		long B = Long.parseLong(tmp.split(" ")[1]);
+		long C = 0, D = 0;
 
-		C = (int)(Math.sqrt(B)) - (int)(Math.sqrt(A));
+		for (long i = (long)(Math.sqrt(A)); i*i <= B; i++) {
+			C = i*i > A ? 1 : 0;
+		}
 		
-		if (C == 0) {
+		if (C == 0L) {
 			System.out.println("0");
 		}
 		else {
-			D = GCD(C, B-A);
-			System.out.println(C/D + "/" + (B-A)/D);
+			D = GCD(B-A, C);
+			System.out.println((C/D) + "/" + ((B-A)/D));
 		}
 	}
 }
