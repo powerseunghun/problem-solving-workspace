134a8fd22fe13fc4a7b1aa6ce5367449c6194a14
powerseunghun <size411@naver.com>
2023-01-06 13:48:45 +0900

    b3 as ap

--- src/Acmicpc/As/B3/TwentyThousandth/Zero/Zero/Топоткотов20017.java
+++ src/Acmicpc/As/B3/TwentyThousandth/Zero/Zero/Топоткотов20017.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Топоткотов20017 {
-	static int[][] arr = null;
+	static int[] arr = null;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String tmp = br.readLine();
@@ -14,18 +14,14 @@ public class Топоткотов20017 {
 		int n = Integer.parseInt(tmp.split(" ")[0]);
 		int m = Integer.parseInt(tmp.split(" ")[1]);
 		int a = Integer.parseInt(tmp.split(" ")[2]), res = 0;
-		arr = new int[n][m];
+		arr = new int[n*m];
 		
-		for (int i = arr.length-1; i >= 0; i--) {
-			for (int j = 0; j < arr[i].length; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for (int i = arr.length-1; i > 0; i--) {
-			for (int j = 0; j < arr[i].length; j++) {
-				if (arr[i-1][j] > arr[i][j] * 2) res+=a;
-			}
+		for (int i = arr.length-1; i-m >= 0; i--) {
+			if (arr[i] > arr[i-m] * 2) res+=a;
 		}
 		
 		System.out.println(res);
