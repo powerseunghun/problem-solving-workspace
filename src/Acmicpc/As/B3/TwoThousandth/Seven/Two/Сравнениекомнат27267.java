df4517cc3332a9644ea6e9d0c44bbbdb4b94052a
anseunghun <size411@naver.com>
2023-01-16 23:38:16 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/TwoThousandth/Seven/Two/Сравнениекомнат27267.java
@@ -0,0 +1,19 @@
+package Acmicpc.As.B3.TwoThousandth.Seven.Two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Сравнениекомнат27267 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String tmp = br.readLine();
+		int a = Integer.parseInt(tmp.split(" ")[0]);
+		int b = Integer.parseInt(tmp.split(" ")[1]);
+		int c = Integer.parseInt(tmp.split(" ")[2]);
+		int d = Integer.parseInt(tmp.split(" ")[3]);
+		
+		System.out.println((a*b == c*d) ? "E" : (a*b > c*d) ? "M" : "P");
+		br.close();
+	}
+}
