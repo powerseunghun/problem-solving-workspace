e68ddb96473fbe37ad33d5065eb44603d3c4b68b
anseunghun <size411@naver.com>
2023-01-28 17:40:19 +0900

    b2 as

---
+++ src/Acmicpc/As/B2/TwenThousandth/Four/Seven/BreakdownClock14710.java
@@ -0,0 +1,17 @@
+package Acmicpc.As.B2.TwenThousandth.Four.Seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BreakdownClock14710 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String tmp = br.readLine();
+		int h = Integer.parseInt(tmp.split(" ")[0]);
+		int m = Integer.parseInt(tmp.split(" ")[1]);
+		
+		System.out.println(h%30*12 == m ? "O" : "X");
+		br.close();
+	}
+}
