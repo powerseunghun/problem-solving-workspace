cc7638c2cee49f7219aa5a545b4316abc869dcbc
anseunghun <size411@naver.com>
2023-02-02 00:14:15 +0900

    b2 as

---
+++ src/Acmicpc/As/B2/TwentyThousandth/One/One/MissingNumber21144.java
@@ -0,0 +1,35 @@
+package Acmicpc.As.B2.TwentyThousandth.One.One;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MissingNumber21144 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		String str = br.readLine();
+		int target = 9, w = 1, res = 0;
+		boolean flag = false;
+		
+		for (int i = 0, n = 1, val = 0; i < str.length(); i+=w) {
+			res++;
+			val = Integer.parseInt(str.substring(i, i+w));
+			System.out.println("val : " + val);
+			if (n != val) {
+				flag = true;
+				break;
+			}
+			n++;
+			if (val == target) {
+				i--;
+				w++;
+				target *= 10;
+				target += 9;
+			}
+		}
+		
+		System.out.println(flag ? res : res+1);
+		br.close();
+	}
+}
