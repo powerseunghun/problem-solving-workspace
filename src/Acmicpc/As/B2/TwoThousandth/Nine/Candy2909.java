558e600a3134711e28fc37d795d2a00a7af60afd
powerseunghun <size411@naver.com>
2023-02-05 13:41:55 +0900

    b2 as

--- src/Acmicpc/As/B2/TwoThousandth/Nine/Candy2909.java
+++ src/Acmicpc/As/B2/TwoThousandth/Nine/Candy2909.java
@@ -12,8 +12,9 @@ public class Candy2909 {
 		int K = Integer.parseInt(tmp.split(" ")[1]);
 		int z = (int)Math.pow(10, K), tc = C/z*z;
 		
+		System.out.println("z : " + z);
 		// 반올
-		System.out.println(C%z >= z/2 ? z+tc : tc);
+		System.out.println(C%z >= z/2 ? (z==1 ? 0 : z)+tc : tc);
 		br.close();
 	}
 }
