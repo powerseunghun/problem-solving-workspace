141d9daf9dadfe549e88addffa9e81947c0819f4
powerseunghun <size411@naver.com>
2023-02-11 18:34:14 +0900

    b2 as

---
+++ src/Acmicpc/As/B2/ThreeThousandth/Two/Rooms3282.java
@@ -0,0 +1,61 @@
+package Acmicpc.As.B2.ThreeThousandth.Two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Rooms3282 {
+	static int[] arr = null;
+	static void func(int C) {
+		while(C > 0) {
+			System.out.println("c: " + C);
+			boolean flag = false;
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] == 0 && C >= 2) {
+					arr[i] += 2;
+					C-=2;
+					flag = true;
+					break;
+				}
+			}
+			if(!flag) {
+				for (int i = 0; i < arr.length; i++) {
+					if (arr[i] == 0) {
+						arr[i]++;
+						C--;
+						flag = true;
+						break;
+					}
+				}
+			}
+			if (!flag) {
+				for (int i = 0; i < arr.length; i++) {
+					if (arr[i] < 2) {
+						arr[i]++;
+						C--;
+						break;
+					}
+				}
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String tmp = br.readLine();
+		int N = Integer.parseInt(tmp.split(" ")[0]);
+		int G = Integer.parseInt(tmp.split(" ")[1]), C = 0;
+		arr = new int[N];
+		
+		while(G-- > 0) {
+			C = Integer.parseInt(br.readLine());
+			func(C);
+		}
+		for (int el : arr) {
+			sb.append(el + " ");
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+}
