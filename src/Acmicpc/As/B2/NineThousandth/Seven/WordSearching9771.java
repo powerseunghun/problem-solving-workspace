35567f9a5544e306708e7585615f17afb88c8ee4
powerseunghun <size411@naver.com>
2023-03-01 15:40:25 +0900

    b2 as

---
+++ src/Acmicpc/As/B2/NineThousandth/Seven/WordSearching9771.java
@@ -0,0 +1,23 @@
+package Acmicpc.As.B2.NineThousandth.Seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class WordSearching9771 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine(), tmp = null;
+		int cnt = 0;
+		
+		while((tmp = br.readLine()) != null) {
+			for (int i = 0; i < tmp.split(" ").length; i++) {
+				if ((tmp.split(" ")[i]).contains(str)) cnt++;
+			}
+			System.out.println("cnt : " + cnt);
+		}
+		
+		System.out.println(cnt);
+		br.close();
+	}
+}
