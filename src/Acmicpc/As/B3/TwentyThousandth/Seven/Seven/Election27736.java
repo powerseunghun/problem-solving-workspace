cccef7645df1c8583f50e3305f935ee408126df0
powerseunghun <size411@naver.com>
2023-03-25 19:09:41 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/TwentyThousandth/Seven/Seven/Election27736.java
@@ -0,0 +1,32 @@
+package Acmicpc.As.B3.TwentyThousandth.Seven.Seven;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Election27736 {
+	static int[] arr = new int[3];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()), v = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		while (st.hasMoreTokens()) {
+			v = Integer.parseInt(st.nextToken());
+			switch(v) {
+			case 1:
+				arr[0]++;
+				break;
+			case -1:
+				arr[1]++;
+				break;
+			default:
+				arr[2]++;
+				break;
+			}
+		}
+		
+		System.out.println(arr[2] >= N/2 ? "INVALID" : (arr[1] > arr[0] || arr[0] == arr[1]) ? "REJECTED" : "APPROVED");
+		br.close();
+	}
+}
