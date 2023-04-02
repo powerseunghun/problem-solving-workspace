5f5897a6048f5c2992745829f558f952b2f16bdb
powerseunghun <size411@naver.com>
2023-04-02 21:40:42 +0900

    b2 as nc

--- src/Acmicpc/As/B2/SixThousandth/Zero/HexadecimalConversion6040.java
+++ src/Acmicpc/As/B2/SixThousandth/Zero/HexadecimalConversion6040.java
@@ -5,12 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class HexadecimalConversion6040 {
-	static String formatting(String str) {
-		while (str.length() % 4 != 0) {
-			str = "0" + str;
-		}
-		return str;
-	}
 	static int charParse(char c) {
 		return c >= '0' && c <= '9' ? c-'0' : c-55;
 	}
@@ -21,30 +15,22 @@ public class HexadecimalConversion6040 {
 		}
 		return sum;
 	}
-	static String getOctaValue(String hexStr) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0, w = 0; i < hexStr.length(); i+= 3) {
-			w = getVal(hexStr.substring(i, i+3));
-			if (w == 0) continue;
-			sb.append(w);
-		}
-		return sb.toString();
-	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine(), hexStr = null, res = null;
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) == ' ') continue;
-			sb.append(formatting(Integer.toBinaryString(charParse(str.charAt(i)))));
-		}
-		hexStr = sb.toString();
-		while (hexStr.length() % 3 != 0) {
-			hexStr = "0" + hexStr;
-		}
-		
-		res = getOctaValue(hexStr);
-		System.out.println(res.replaceAll("0", "").length() == 0 ? "0" : res);
-		br.close();
+		  String str = br.readLine(), res = "";
+		  int idx = str.length()-1;
+		  long sum = 0;
+		  
+		  for (int i = 0; i < str.length(); i++) {
+			  sum += charParse(str.charAt(i)) * Math.pow(16, idx--);
+		  }
+		  
+		  while (sum > 0) {
+			  res = sum % 8 + res;
+			  sum /= 8;
+		  }
+		  
+		  System.out.println(res.equals("") ? "0" : res);
+		  br.close();
 	}
 }
