9bc1c9f81916433259cadc26f784888deafa8ae3
powerseunghun <size411@naver.com>
2023-04-22 16:16:50 +0900

    b3 as ap

--- src/Acmicpc/As/B3/TenThousandth/Nine/Five/Drought19572.java
+++ src/Acmicpc/As/B3/TenThousandth/Nine/Five/Drought19572.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Drought19572 {
 	static final int small = 3;
-	static int[] arr = null;
+	static double[] arr = null;
 	static String check(double a, double b, double c) {
 		StringBuilder sb = new StringBuilder();
 		if (a <= 0 || b <= 0 || c <= 0) {
@@ -15,24 +15,24 @@ public class Drought19572 {
 			return sb.toString();
 		}
 		sb.append("1").append("\n");
-		sb.append(String.format("%.1f, %.1f, %.1f", a, b, c)).append("\n");
+		sb.append(String.format("%.1f %.1f %.1f", a, b, c)).append("\n");
 		return sb.toString();
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		double a = 0, b = 0, c = 0;
-		arr = new int[small];
+		double a = 0, b = 0, c = 0, sum = 0;
+		arr = new double[small];
 		
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Double.parseDouble(st.nextToken());
+			sum += arr[i];
 		}
-		b = arr[0]-arr[1]+arr[2];
-		b /= 2.0;
-		a = Math.abs(arr[0]-b);
-		c = Math.abs(arr[2]-b);
+		a = (sum / 2) - arr[2];
+		b = (sum / 2) - arr[1];
+		c = (sum / 2) - arr[0];
 		
-		System.out.println(check(a, b, c));
+		System.out.print(check(a, b, c));
 		br.close();
 	}
 }
