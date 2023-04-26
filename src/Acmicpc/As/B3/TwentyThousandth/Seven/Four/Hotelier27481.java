b0747fbb45d3470c46cbc616b4fae93ce0c38849
powerseunghun <size411@naver.com>
2023-04-26 22:27:34 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/TwentyThousandth/Seven/Four/Hotelier27481.java
@@ -0,0 +1,34 @@
+package Acmicpc.As.B3.TwentyThousandth.Seven.Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Hotelier27481 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		String str = br.readLine();
+		StringBuilder sb = new StringBuilder("0000000000");
+		char c = ' ';
+		
+		System.out.println(str.length());
+		for (int i = 0, idx = 0, flag = 0; i < str.length(); i++, flag = 0) {
+			c = str.charAt(i);
+			switch(c) {
+			case 'L': case 'R':
+				idx = c == 'L' ? sb.toString().indexOf('0') : sb.toString().lastIndexOf('0');
+				sb.setCharAt(idx, '1');
+				break;
+			default:
+				idx = c-'0';
+				flag = 1;
+				break;
+			}
+			sb.setCharAt(idx, flag == 0 ? '1' : '0');
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+}
