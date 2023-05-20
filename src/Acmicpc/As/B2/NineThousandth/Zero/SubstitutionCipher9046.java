1b15895def697fefdb859856ac67292ff96b6d6a
powerseunghun <size411@naver.com>
2023-05-20 23:21:59 +0900

    b2 as

---
+++ src/Acmicpc/As/B2/NineThousandth/Zero/SubstitutionCipher9046.java
@@ -0,0 +1,47 @@
+package Acmicpc.As.B2.NineThousandth.Zero;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubstitutionCipher9046 {
+	static int[] cnts = null;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine()), max = 0, cnt = 0;
+		String str = null;
+		
+		while(N-- > 0) {
+			str = br.readLine();
+			cnts = new int[26];
+			max = 0;
+			cnt = 0;
+			for (int i = 0, w = 0; i < str.length(); i++) {
+				if (str.charAt(i) == ' ') continue;
+				w = str.charAt(i) - 97;
+				System.out.println("w : " + w);
+				cnts[w]++;
+				max = Math.max(cnts[w], max);
+			}
+			for (int el : cnts) {
+				if (el == max) cnt++;
+			}
+			if (cnt > 1) {
+				sb.append("?").append("\n");
+			}
+			else {
+				for (int i = 0; i < cnts.length; i++) {
+					if (cnts[i] == max) {
+						sb.append((char)(i+97)).append("\n");
+					}
+				}
+			}
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
