dea2848f1585401006e04d942e3dd6ed870c7bc9
powerseunghun <size411@naver.com>
2023-05-25 21:07:12 +0900

    b2 as

---
+++ src/Acmicpc/As/B2/NineThousandth/Two/GrandingExam9296.java
@@ -0,0 +1,31 @@
+package Acmicpc.As.B2.NineThousandth.Two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class GrandingExam9296 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine()), c = 1, cnt = 0;
+		String str1 = null, str2 = null;
+		
+		while(T-- > 0) {
+			br.readLine();
+			cnt = 0;
+			str1 = br.readLine();
+			str2 = br.readLine();
+			
+			for (int i = 0; i < str1.length(); i++) {
+				if (str1.charAt(i) != str2.charAt(i)) cnt++;
+			}
+			
+			sb.append("Case " + (c++) + ": " + cnt).append("\n");
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
