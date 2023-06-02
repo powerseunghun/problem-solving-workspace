d235dcd0a6ddd8e14f6e4aac63776355d1916985
powerseunghun <size411@naver.com>
2023-06-02 22:20:49 +0900

    b2 as ap

---
+++ src/Acmicpc/As/B2/TwentyThousandth/Seven/Three/ClassFieldTrip27386.java
@@ -0,0 +1,35 @@
+package Acmicpc.As.B2.TwentyThousandth.Seven.Three;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import Acmicpc.Sorting.TwoThousandth.Nine.FeequencySort2910;
+
+public class ClassFieldTrip27386 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		List<Character> list = new ArrayList<>();
+		String str = null;
+		
+		for (int i = 0; i < 2; i++) {
+			str = br.readLine();
+			for (int j = 0; j < str.length(); j++) {
+				list.add(str.charAt(j));
+			}
+		}
+		Collections.sort(list);
+		
+		for (char c : list) {
+			sb.append(c);
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+}
