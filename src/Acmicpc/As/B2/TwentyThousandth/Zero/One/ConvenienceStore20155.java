9824d58048ba571b8676215e2334d363b5003e2c
powerseunghun <size411@naver.com>
2023-06-28 21:23:14 +0900

    b2 as

---
+++ src/Acmicpc/As/B2/TwentyThousandth/Zero/One/ConvenienceStore20155.java
@@ -0,0 +1,26 @@
+package Acmicpc.As.B2.TwentyThousandth.Zero.One;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ConvenienceStore20155 {
+	static int[] res = null;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(str.split(" ")[0]);
+		int M = Integer.parseInt(str.split(" ")[1]);
+		int m = Integer.MIN_VALUE, v = 0;
+		
+		res = new int[M+1];
+		
+		while (st.hasMoreTokens()) {
+			m = Math.max(m, ++res[Integer.parseInt(st.nextToken())]);
+		}
+		
+		System.out.println(m);
+		br.close();
+	}
+}
