7018f8c86b9a030464848b56d73abd73ae73e138
powerseunghun <size411@naver.com>
2023-08-16 23:23:11 +0900

    b1 as

---
+++ src/Acmicpc/As/B1/TwoThousandth/Two/MachinCode2929.java
@@ -0,0 +1,23 @@
+package Acmicpc.As.B1.TwoThousandth.Two;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MachinCode2929 {
+	static final int len = 4;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int res = 0;
+		
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (c >= 'A' && c <= 'Z') {
+				while ((i + res) % len != 0) res++;
+			}
+		}
+		
+		System.out.println(res);
+		br.close();
+	}
+}
