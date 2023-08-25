8db5b625cee3e637baab1f68d08f677f5cbc2276
powerseunghun <size411@naver.com>
2023-08-25 18:04:29 +0900

    b1 as

--- src/Acmicpc/As/B1/EightThousandth/Five/HiddenNumber8595.java
+++ src/Acmicpc/As/B1/EightThousandth/Five/HiddenNumber8595.java
@@ -3,19 +3,19 @@ package Acmicpc.As.B1.EightThousandth.Five;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class HiddenNumber8595 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = null;
 		br.readLine();
-		String str = br.readLine();
-		String[] tokens = str.split("[a-z]|[A-Z]");
-		int res = 0;
+		String str = br.readLine().replaceAll("[^0-9]", " ");
+		StringTokenizer st = new StringTokenizer(str, " ");
+		long res = 0;
 		
-		for (String s : tokens) {
-			if (!s.equals("")) {
-				res += Integer.parseInt(s);
-			}
+		while (st.hasMoreTokens()) {
+			res += Long.parseLong(st.nextToken());
 		}
 		
 		System.out.println(res);
