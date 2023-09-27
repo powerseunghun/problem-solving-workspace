c9b1c3662afd00a0f5b31136a6dc5f528e2e6900
powerseunghun <size411@naver.com>
2023-09-28 04:00:31 +0900

    b1 as

---
+++ src/Acmicpc/As/B1/TwentyThousandth/Eight/Zero/CoinCopy28063.java
@@ -0,0 +1,14 @@
+package Acmicpc.As.B1.TwentyThousandth.Eight.Zero;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CoinCopy28063 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		int x = Integer.parseInt(str.split(" ")[0]);
+		int y = Integer.parseInt(str.split(" ")[1]);
+	}
+}
