c4bb512601dd1bccd73ce1603f9e7fb2bce8e277
powerseunghun <size411@naver.com>
2023-10-24 07:40:43 +0900

    s5 as

---
+++ src/Acmicpc/As/S5/TwoThousandth/Zero/NumbersSum2018.java
@@ -0,0 +1,35 @@
+package Acmicpc.As.S5.TwoThousandth.Zero;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumbersSum2018 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int l = 0, r = 0, sum = 0, res = 0;
+		
+		while(l <= N) {
+			while (r++ <= N) {
+				sum += r;
+				System.out.println("sum : " + sum);
+				if (sum >= N) {
+					res = sum == N ? res+1 : res;
+					break;
+				}
+			}
+			while(l++ <= N) {
+				sum -= l;
+				System.out.println("sum : " + sum);
+				if (sum <= N) {
+					res = sum == N ? res+1 : res;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(res);
+		br.close();
+	}
+}
