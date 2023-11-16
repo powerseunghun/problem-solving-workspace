0660eccaf4313d636b3bd56b05f02833f447bb3a
powerseunghun <size411@naver.com>
2023-11-17 01:20:27 +0900

    s5 as tle

---
+++ src/Acmicpc/As/B5/TenThousandth/Four/Seven/YouSeeLab14729.java
@@ -0,0 +1,27 @@
+package Acmicpc.As.B5.TenThousandth.Four.Seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class YouSeeLab14729 {
+	static final int st = 7;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Double> pq = new PriorityQueue<Double>();
+		
+		while(N-- > 0) {
+			pq.add(Double.parseDouble(br.readLine()));
+		}
+		
+		for (int i = 0; i < st; i++) {
+			sb.append(pq.poll()).append("\n");
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
