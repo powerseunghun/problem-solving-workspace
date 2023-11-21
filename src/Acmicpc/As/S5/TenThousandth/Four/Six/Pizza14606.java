015559f4f981116972fd32a37829490efab326f0
powerseunghun <size411@naver.com>
2023-11-21 22:32:18 +0900

    s5 as

---
+++ src/Acmicpc/As/S5/TenThousandth/Four/Six/Pizza14606.java
@@ -0,0 +1,23 @@
+package Acmicpc.As.S5.TenThousandth.Four.Six;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Pizza14606 {
+	static final int limit = 10;
+	static int[] arr = new int[limit+1];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		arr[2] = 1;
+		for (int i = 3; i < arr.length; i++) {
+			arr[i] = arr[i-1] + i-1;
+		}
+		
+		System.out.println(arr[N]);
+		br.close();
+	}
+}
