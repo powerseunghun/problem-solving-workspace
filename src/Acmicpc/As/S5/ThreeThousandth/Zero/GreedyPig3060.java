35cab6e2fe193f49bbbba529e52d7587f3655861
powerseunghun <size411@naver.com>
2023-12-03 03:08:32 +0900

    s5 as

---
+++ src/Acmicpc/As/S5/ThreeThousandth/Zero/GreedyPig3060.java
@@ -0,0 +1,33 @@
+package Acmicpc.As.S5.ThreeThousandth.Zero;
+
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class GreedyPig3060 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		int T = Integer.parseInt(br.readLine()), N = 0, s = 0, res = 0;
+		
+		while(T-- > 0) {
+			N = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			while(st.hasMoreTokens()) {
+				s += Integer.parseInt(st.nextToken());
+			}
+			
+			res = 1;
+			while (s <= N) {
+				s *= 4;
+				res++;
+			}
+			sb.append(res).append("\n");
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
