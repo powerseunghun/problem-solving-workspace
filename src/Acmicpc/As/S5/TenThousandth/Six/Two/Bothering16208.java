19c52eee8a665207ba1723743748860e80a2c3ed
powerseunghun <size411@naver.com>
2023-12-14 00:00:25 +0900

    s5 as

---
+++ src/Acmicpc/As/S5/TenThousandth/Six/Two/Bothering16208.java
@@ -0,0 +1,10 @@
+package Acmicpc.As.S5.TenThousandth.Six.Two;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bothering16208 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
