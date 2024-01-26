4089098cbdb1fa3c4dda1b3b5fe1a01ec0e1091b
powerseunghun <size411@naver.com>
2024-01-26 22:45:27 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/TwentyThousandth/Seven/Three/Square27323.java
@@ -0,0 +1,16 @@
+package Acmicpc.As.B5.TwentyThousandth.Seven.Three;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Square27323 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		
+		System.out.println(A * B);
+		br.close();
+	}
+}
