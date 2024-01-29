9dc168c91f4d4cecbe362a77af044dc141b17fc9
powerseunghun <size411@naver.com>
2024-01-29 19:28:29 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/TwentyThousandth/Seven/Three/Metronome27389.java
@@ -0,0 +1,14 @@
+package Acmicpc.As.B5.TwentyThousandth.Seven.Three;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Metronome27389 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		double res = Integer.parseInt(br.readLine()) / 4.0;
+		System.out.println(res);
+		br.close();
+	}
+}
