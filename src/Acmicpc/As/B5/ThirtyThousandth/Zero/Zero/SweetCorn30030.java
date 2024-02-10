26f6f0875fd17e60ad3cdf5e19022018549e1908
powerseunghun <size411@naver.com>
2024-02-10 21:45:47 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Zero/Zero/SweetCorn30030.java
@@ -0,0 +1,11 @@
+package Acmicpc.As.B5.ThirtyThousandth.Zero.Zero;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SweetCorn30030 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println(Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine()) / 11 * 10);
+	}
+}
