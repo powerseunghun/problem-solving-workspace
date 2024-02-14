2b3cec796eec863cbd6322e04e43ee8aafdb7d47
powerseunghun <size411@naver.com>
2024-02-14 23:31:28 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Zero/Three/JavaWarriors30328.java
@@ -0,0 +1,14 @@
+package Acmicpc.As.B5.ThirtyThousandth.Zero.Three;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class JavaWarriors30328 {
+	static final int p = 4000;
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(Integer.parseInt(br.readLine()) * p);
+		br.close();
+	}
+}
