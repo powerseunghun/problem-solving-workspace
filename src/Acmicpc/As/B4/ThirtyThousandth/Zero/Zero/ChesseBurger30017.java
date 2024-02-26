e3999b041e0df8e06c51d172f812b230771e306f
powerseunghun <size411@naver.com>
2024-02-26 20:29:06 +0900

    b4 as ap

---
+++ src/Acmicpc/As/B4/ThirtyThousandth/Zero/Zero/ChesseBurger30017.java
@@ -0,0 +1,19 @@
+package Acmicpc.As.B4.ThirtyThousandth.Zero.Zero;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ChesseBurger30017 {
+	static final int base = 3;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int A = Integer.parseInt(str.split(" ")[0]) - 2;
+		int B = Integer.parseInt(str.split(" ")[1]) - 1;
+		
+		System.out.println(base + Math.min(A, B) * 2);
+		br.close();
+	}
+}
