11ff51550a7006c28b621054ddef34ea1b64c40a
powerseunghun <size411@naver.com>
2024-03-03 09:28:36 +0900

    b4 as ap

---
+++ src/Acmicpc/As/B4/TwentyThousandth/Nine/Sevenn/FindDKSH29766.java
@@ -0,0 +1,16 @@
+package Acmicpc.As.B4.TwentyThousandth.Nine.Sevenn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FindDKSH29766 {
+	static final String t = "DKSH";
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		System.out.println((str.length() - str.replaceAll(t, "").length()) / t.length());
+		br.close();
+	}
+}
