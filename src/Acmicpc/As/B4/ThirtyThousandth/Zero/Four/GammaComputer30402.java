fa7f23c0cb028d0dc1990dc933364d9fa1edd004
powerseunghun <size411@naver.com>
2024-03-05 23:20:43 +0900

    b4 as ap

---
+++ src/Acmicpc/As/B4/ThirtyThousandth/Zero/Four/GammaComputer30402.java
@@ -0,0 +1,29 @@
+package Acmicpc.As.B4.ThirtyThousandth.Zero.Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GammaComputer30402 {
+	static final int line = 15;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = null;
+		int res = 0;
+		for (int i = 0; i < line; i++) {
+			str = br.readLine();
+			if (str.contains("w")) {
+				res = 1;
+			}
+			else if (str.contains("b")) {
+				res = 2;
+			}
+			else if (str.contains("g")) {
+				res = 3;
+			}
+		}
+		
+		System.out.println(res == 1 ? "chunbae" : res == 1 ? "nabi" : "yeongcheol");
+		br.close();
+	}
+}
