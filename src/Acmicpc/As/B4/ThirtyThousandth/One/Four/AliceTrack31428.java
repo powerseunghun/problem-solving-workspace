bca26e0b918e565707c3af4dca508637fcf14a78
powerseunghun <size411@naver.com>
2024-03-22 21:00:52 +0900

    b4 as ap

---
+++ src/Acmicpc/As/B4/ThirtyThousandth/One/Four/AliceTrack31428.java
@@ -0,0 +1,22 @@
+package Acmicpc.As.B4.ThirtyThousandth.One.Four;
+
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class AliceTrack31428 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String str = br.readLine();
+		int res = 0;
+		
+		while (st.hasMoreTokens()) {
+			res += st.nextToken().equals(str) ? 1 : 0;
+		}
+		
+		System.out.println(res);
+		br.close();
+	}
+}
