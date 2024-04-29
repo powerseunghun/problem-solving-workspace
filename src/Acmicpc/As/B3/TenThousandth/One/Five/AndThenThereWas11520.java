0a5c44e7a32fec191ced9be9e180d8ffa61675d0
powerseunghun <size411@naver.com>
2024-04-29 19:51:04 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/TenThousandth/One/Five/AndThenThereWas11520.java
@@ -0,0 +1,10 @@
+package Acmicpc.As.B3.TenThousandth.One.Five;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class AndThenThereWas11520 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
