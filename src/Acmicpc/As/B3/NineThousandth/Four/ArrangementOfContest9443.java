904357762ed883fa3f2e62fe34029c2eefc6fd05
powerseunghun <size411@naver.com>
2024-05-08 18:39:05 +0900

    b3 as ap

--- src/Acmicpc/As/B3/NineThousandth/Four/ArrangementOfContest9443.java
+++ src/Acmicpc/As/B3/NineThousandth/Four/ArrangementOfContest9443.java
@@ -3,27 +3,25 @@ package Acmicpc.As.B3.NineThousandth.Four;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArrangementOfContest9443 {
-	static int[] arr = new int[26];
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine()), cnt = 0, max = 0;
-		for (int i = 0; i < N; i++) {
-			arr[br.readLine().charAt(0)-65]++;
+		List<Character> list = new ArrayList<Character>();
+		int N = Integer.parseInt(br.readLine()), res = 0;
+		
+		while(N-- > 0) {
+			list.add(br.readLine().charAt(0));
 		}
 		
-		for (int el : arr) {
-			if (el >= 1) {
-				cnt++;
-				continue;
-			}
-			max = Math.max(max, cnt);
-			cnt = 0;
-			break;
+		for (int i = 65; i <= 90; i++) {
+			System.out.println((char)i);
+			if (!list.contains((char)i)) break;
+			res++;
 		}
-
-		System.out.println(max);
+		System.out.println(res);
 		br.close();
 	}
 }
