c72eb5426702043666cec098bcc643f4552ea2a9
powerseunghun <size411@naver.com>
2024-05-15 19:34:30 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B4/ThirtyThousandth/One/Six/StickyKeys31656.java
@@ -0,0 +1,22 @@
+package Acmicpc.As.B4.ThirtyThousandth.One.Six;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StickyKeys31656 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(str.charAt(0));
+		for (int i = 1; i < str.length(); i++) {
+			if (str.charAt(i) == str.charAt(i-1)) continue;
+			sb.append(str.charAt(i));
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
+	}
+}
