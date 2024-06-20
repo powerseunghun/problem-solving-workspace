46258afdccfe951e04898f0416269d7cb0056b7c
powerseunghun <size411@naver.com>
2024-06-20 21:57:56 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/TenThousandth/Nine/Seven/Сапсан19771.java
@@ -0,0 +1,14 @@
+package Acmicpc.As.B3.TenThousandth.Nine.Seven;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Сапсан19771 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println(N/6*4);
+		br.close();
+	}
+}
