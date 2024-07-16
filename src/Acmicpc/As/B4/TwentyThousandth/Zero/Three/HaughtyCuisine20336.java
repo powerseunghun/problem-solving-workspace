2698760cc2e3ced0e7cb3eebf013b55541257f36
powerseunghun <size411@naver.com>
2024-07-16 20:02:31 +0900

    b4 as ap

---
+++ src/Acmicpc/As/B4/TwentyThousandth/Zero/Three/HaughtyCuisine20336.java
@@ -0,0 +1,20 @@
+package Acmicpc.As.B3.TwentyThousandth.Zero.Three;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class HaughtyCuisine20336 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		while(st.hasMoreTokens()) {
+			System.out.println(st.nextToken());
+		}
+		
+		br.close();
+	}
+}
