b15a368097a067f84a4177a0b5fefb5327533d25
powerseunghun <size411@naver.com>
2024-07-22 21:16:56 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/TwentyThousandth/Five/Six/OddEvenArray25629.java
@@ -0,0 +1,24 @@
+package Acmicpc.As.B3.TwentyThousandth.Five.Six;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class OddEvenArray25629 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int v = 0, odd = 0, even = 0;
+		
+		while(st.hasMoreTokens()) {
+			v = Integer.parseInt(st.nextToken());
+			if ((v&1) == 1) odd++;
+			else even++;
+		}
+		
+		System.out.println(odd + " " + even);
+		System.out.println((odd-even == 0 || odd-even == 1) ? "1" : "0");
+		br.close();
+	}
+}
