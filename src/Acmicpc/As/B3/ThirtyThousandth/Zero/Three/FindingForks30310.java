202cabdf16362b4f445f3fbe279b110f92d67685
powerseunghun <size411@naver.com>
2024-08-21 00:41:32 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/ThirtyThousandth/Zero/Three/FindingForks30310.java
@@ -0,0 +1,23 @@
+package Acmicpc.As.B3.ThirtyThousandth.Zero.Three;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class FindingForks30310 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[Integer.parseInt(br.readLine())];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0]+arr[1]);
+		br.close();
+	}
+}
