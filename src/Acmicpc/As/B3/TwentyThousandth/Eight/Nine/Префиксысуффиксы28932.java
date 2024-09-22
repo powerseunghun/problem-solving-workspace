795440d40525a98b7f12a303993283b0c63203a0
powerseunghun <size411@naver.com>
2024-09-22 18:02:20 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/TwentyThousandth/Eight/Nine/Префиксысуффиксы28932.java
@@ -0,0 +1,16 @@
+package Acmicpc.As.B3.TwentyThousandth.Eight.Nine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Префиксысуффиксы28932 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		br.readLine();
+		
+		System.out.println("1 1");
+		br.close();
+	}
+}
