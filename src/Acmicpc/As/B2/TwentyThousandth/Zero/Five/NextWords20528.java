3c8f53f38283be71fa014f104b15c283ee80b568
powerseunghun <size411@naver.com>
2024-10-01 19:03:30 +0900

    b2 as ap

---
+++ src/Acmicpc/As/B2/TwentyThousandth/Zero/Five/NextWords20528.java
@@ -0,0 +1,24 @@
+package Acmicpc.As.B2.TwentyThousandth.Zero.Five;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NextWords20528 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String b = st.nextToken(), s = null;
+		boolean flag = true;
+		
+		while(flag && st.hasMoreTokens()) {
+			s = st.nextToken();
+			if (b.charAt(0) != s.charAt(0)) flag = false;
+		}
+		
+		System.out.println(flag ? "1" : "0");
+		br.close();
+	}
+}
