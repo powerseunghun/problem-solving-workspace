4e6fb468ab018c93ecec668d33ccbb03f4263f96
powerseunghun <size411@naver.com>
2024-10-21 18:24:39 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B4/TwentyThousandth/Eight/Eight/Таблицарезультатов28808.java
@@ -0,0 +1,22 @@
+package Acmicpc.As.B4.TwentyThousandth.Eight.Eight;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Таблицарезультатов28808 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine().split(" ")[0]);
+		int res = 0;
+		String str = null;
+		
+		while(n-- > 0) {
+			str = br.readLine();
+			res = str.contains("+") ? res+1 : res;
+		}
+		
+		System.out.println(res);
+		br.close();
+	}
+}
