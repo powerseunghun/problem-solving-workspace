fd4653e6e72869dfbd04edd48f8bf054388a4fea
powerseunghun <size411@naver.com>
2024-11-17 13:54:26 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Two/Four/JustRoundDown32458.java
@@ -0,0 +1,13 @@
+package Acmicpc.As.B5.ThirtyThousandth.Two.Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JustRoundDown32458 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println((int)Double.parseDouble(br.readLine()));
+		br.close();
+	}
+}
