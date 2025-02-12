b76c9265718c58feb31214805d804af4422f44d0
powerseunghun <size411@naver.com>
2025-02-12 22:02:20 +0900

    b2 as ap tle

---
+++ src/Acmicpc/As/B2/ThirtyThousandth/Two/One/CardGame32141.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CardGame32141 {
+	static int[] arr = null;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int N = Integer.parseInt(str.split(" ")[0]);
+		int H = Integer.parseInt(str.split(" ")[1]), idx = 0, v = 0, res = -1;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		
+		while(st.hasMoreTokens()) {
+			v = Integer.parseInt(st.nextToken());
+			arr[idx++] = v;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			H -= arr[i];
+			if (H <= 0) res = i+1;
+		}
+		
+		System.out.println(res);
+		br.close();
+	}
+}
