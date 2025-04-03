17a2807a0bcd3aa9b006575685fefd54e6550355
powerseunghun <size411@naver.com>
2025-04-03 19:51:41 +0900

    b2 as ap

---
+++ src/Acmicpc/As/B2/TenThousandth/Two/Five/CentauriPrime12517.java
@@ -0,0 +1,42 @@
+package Acmicpc.As.B2.TenThousandth.Two.Five;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CentauriPrime12517 {
+	static int check(String str) {
+		char c = str.charAt(str.length()-1);
+		if (c == 'y') {
+			return 1;
+		} else if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
+			return 2;
+		} else return 3;
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		String str = null;
+		
+		for (int i = 1; i <= N; i++) {
+			str = br.readLine();
+			sb.append("Case #").append(i).append(": " + str).append(" is ruled by ");
+			switch (check(str)) {
+			case 1:
+				sb.append("nobody");
+				break;
+			case 2:
+				sb.append("a queen");
+				break;
+			case 3:
+				sb.append("a king");
+				break;
+			}
+			sb.append(".\n");
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
