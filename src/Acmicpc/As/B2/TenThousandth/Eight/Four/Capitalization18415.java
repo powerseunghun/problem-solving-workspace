53c2a6bc123ab65df76a84edfd56e10fa17622f6
powerseunghun <size411@naver.com>
2025-04-15 19:10:01 +0900

    b2 as ap

---
+++ src/Acmicpc/As/B2/TenThousandth/Eight/Four/Capitalization18415.java
@@ -0,0 +1,15 @@
+package Acmicpc.As.B2.TenThousandth.Eight.Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Capitalization18415 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		String str = br.readLine();
+		System.out.println(str.replaceAll("joi", "JOI"));
+		br.close();
+	}
+}
