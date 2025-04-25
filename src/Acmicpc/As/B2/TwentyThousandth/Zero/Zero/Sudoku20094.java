eedd6b2a692cdb24eb20fa0e37a125d324cc02f5
powerseunghun <size411@naver.com>
2025-04-25 16:41:01 +0900

    b2 as ap

---
+++ src/Acmicpc/As/B2/TwentyThousandth/Zero/Zero/Sudoku20094.java
@@ -0,0 +1,10 @@
+package Acmicpc.As.B2.TwentyThousandth.Zero.Zero;
+
+public class Sudoku20094 {
+	public static void main(String[] args) {
+//		3 2 4 1
+//		1 4 2 3
+//		4 3 1 2
+//		2 1 3 4
+	}
+}
