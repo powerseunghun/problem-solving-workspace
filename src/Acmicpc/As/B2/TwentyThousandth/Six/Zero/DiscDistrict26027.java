c1551b819f901f440651b66e8817c41beaae5e59
powerseunghun <size411@naver.com>
2025-05-21 18:13:51 +0900

    b2 as ap

---
+++ src/Acmicpc/As/B2/TwentyThousandth/Six/Zero/DiscDistrict26027.java
@@ -0,0 +1,16 @@
+package Acmicpc.As.B2.TwentyThousandth.Six.Zero;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DiscDistrict26027 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int r = Integer.parseInt(br.readLine());
+		
+		System.out.println(r + " 1");
+		br.close();
+		
+	}
+}
