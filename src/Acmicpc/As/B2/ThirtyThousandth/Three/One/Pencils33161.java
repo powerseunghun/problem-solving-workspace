590657ad9afd7be8686c9e5e95fbc3931f024808
powerseunghun <size411@naver.com>
2025-07-03 16:03:26 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B2/ThirtyThousandth/Three/One/Pencils33161.java
@@ -0,0 +1,14 @@
+package Acmicpc.As.B2.ThirtyThousandth.Three.One;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Pencils33161 {
+	static final int m = 5;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine()) / m);
+		br.close();
+	}
+}
