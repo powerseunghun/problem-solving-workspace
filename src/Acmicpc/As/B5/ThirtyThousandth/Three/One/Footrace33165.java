06242d43f9a44d57a6ac085ff45c7a418b18f7f2
powerseunghun <size411@naver.com>
2025-07-05 20:06:19 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Three/One/Footrace33165.java
@@ -0,0 +1,13 @@
+package Acmicpc.As.B5.ThirtyThousandth.Three.One;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Footrace33165 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine())*Integer.parseInt(br.readLine()));
+		br.close();
+	}
+}
