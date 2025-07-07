31878731dcb3a1ef1703776befd68ec18907cdfb
powerseunghun <size411@naver.com>
2025-07-07 19:03:21 +0900

    5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Three/Five/ThreeNotebook33515.java
@@ -0,0 +1,10 @@
+package Acmicpc.As.B5.ThirtyThousandth.Three.Five;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ThreeNotebook33515 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
