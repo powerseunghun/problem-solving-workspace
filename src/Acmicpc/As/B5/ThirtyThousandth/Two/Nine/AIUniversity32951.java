2e2f9369b5d75de577a301658c9e923191ca838c
powerseunghun <size411@naver.com>
2025-07-09 18:18:49 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Two/Nine/AIUniversity32951.java
@@ -0,0 +1,14 @@
+package Acmicpc.As.B5.ThirtyThousandth.Two.Nine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AIUniversity32951 {
+	static final int l = 2024;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine())-l);
+		br.close();
+	}
+}
