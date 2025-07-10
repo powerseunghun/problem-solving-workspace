a344612a6e555a51eebf152123a31812981ccd44
powerseunghun <size411@naver.com>
2025-07-10 18:29:00 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Two/Two/APlusB32260.java
@@ -0,0 +1,11 @@
+package Acmicpc.As.B5.ThirtyThousandth.Two.Two;
+
+public class APlusB32260 {
+	// ========================= c++17
+//	#include "aplusb.h"
+//
+//	int sum(int A, int B) {
+//	  return A + B - 1;
+//	}
+	// =========================
+}
