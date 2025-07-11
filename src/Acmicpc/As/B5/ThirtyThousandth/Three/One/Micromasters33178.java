5587dfb0fd640a36dec5e00de1abd1cc808efcfc
powerseunghun <size411@naver.com>
2025-07-11 11:12:46 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Three/One/Micromasters33178.java
@@ -0,0 +1,15 @@
+package Acmicpc.As.B5.ThirtyThousandth.Three.One;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Micromasters33178 {
+	static final int l = 10;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(Integer.parseInt(br.readLine())/l);
+		br.close();
+	}
+}
