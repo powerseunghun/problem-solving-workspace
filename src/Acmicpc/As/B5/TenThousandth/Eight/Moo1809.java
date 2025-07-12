65ee0d74fb3fa6a10f74fc1b6d370663922676f0
powerseunghun <size411@naver.com>
2025-07-12 16:55:04 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/TenThousandth/Eight/Moo1809.java
@@ -0,0 +1,12 @@
+package Acmicpc.As.B5.TenThousandth.Eight;
+
+public class Moo1809 {
+	public static void main(String[] args) {
+//		'(___)
+//		(o o)____/
+//		 @@      \
+//		  \ ____,/
+//		  //   //
+//		 ^^   ^^'
+	}
+}
