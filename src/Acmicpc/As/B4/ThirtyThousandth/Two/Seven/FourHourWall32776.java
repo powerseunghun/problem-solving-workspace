4c5e0c5c143712d8f84fdfe0132deffaf2248b1b
powerseunghun <size411@naver.com>
2025-08-03 17:55:04 +0900

    b4 as apg

---
+++ src/Acmicpc/As/B4/ThirtyThousandth/Two/Seven/FourHourWall32776.java
@@ -0,0 +1,19 @@
+package Acmicpc.As.B4.ThirtyThousandth.Two.Seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FourHoursWall32776 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sab = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		int ma = Integer.parseInt(str.split(" ")[0]);
+		int fab = Integer.parseInt(str.split(" ")[1]);
+		int mb = Integer.parseInt(str.split(" ")[2]);
+		
+		System.out.println(sab > ma+fab+mb && sab > 240 ? "flight" : "high speed rail");
+		br.close();
+	}
+}
