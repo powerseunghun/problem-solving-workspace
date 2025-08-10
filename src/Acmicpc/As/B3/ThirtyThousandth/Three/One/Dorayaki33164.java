02d2606ccd6ae9e12a4fc702308ca0fa91c95849
powerseunghun <size411@naver.com>
2025-08-10 23:59:30 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/ThirtyThousandth/Three/One/Dorayaki33164.java
@@ -0,0 +1,9 @@
+package Acmicpc.As.B3.ThirtyThousandth.Three.One;
+
+import java.io.InputStreamReader;
+
+public class Dorayaki33164 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
