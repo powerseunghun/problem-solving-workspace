d2cbfad54e29824393ebe82a82ef9c12f6ef2265
powerseunghun <size411@naver.com>
2025-08-27 18:15:35 +0900

    b3 as ap modify answer

--- src/Acmicpc/As/B3/TenThousandth/One/One/NegativePeopleInDaHouse11161.java
+++ src/Acmicpc/As/B3/TenThousandth/One/One/NegativePeopleInDaHouse11161.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class NegativePeopleInDaHouse11161 {
-	// 4f
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -15,12 +14,13 @@ public class NegativePeopleInDaHouse11161 {
 		while (T-- > 0) {
 			M = Integer.parseInt(br.readLine());
 			total = 0;
+			w = 0;
 			for (int i = 0; i < M; i++) {
 				tmp = br.readLine();
 				P1 = Integer.parseInt(tmp.split(" ")[0]);
 				P2 = Integer.parseInt(tmp.split(" ")[1]);
-				if (P2 > P1) total += P2-(P1+w);
-				else if (P1 > P2) w += P1;
+				w = w-P1+P2;
+				total = Math.max(total, w);
 			}
 			sb.append(total + "\n");
 		}
