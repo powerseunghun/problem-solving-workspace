3e60ab81887f0f8a5b57ada21a858c18bb285a20
powerseunghun <size411@naver.com>
2025-09-20 16:35:22 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Four/Four/FlatterLand34424.java
@@ -0,0 +1,15 @@
+package Acmicpc.As.B5.ThirtyThousandth.Four.Four;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class FlatterLand34424 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int x = Integer.parseInt(br.readLine());
+		
+		System.out.println((n-1)*x);
+		br.close();
+	}
+}
