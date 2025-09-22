ff1cf2d6c59dc35e984db772b71fa1a395316801
powerseunghun <size411@naver.com>
2025-09-22 19:06:18 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Four/Four/MClimb34411.java
@@ -0,0 +1,17 @@
+package Acmicpc.As.B5.ThirtyThousandth.Four.Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MClimb34411 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int m = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		br.readLine();
+		
+		System.out.println(m*n);
+		br.close();
+	}
+}
