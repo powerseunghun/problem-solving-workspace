99310f9bf5b08e6c2072c207914667b88863aa10
powerseunghun <size411@naver.com>
2025-09-23 18:23:49 +0900

    b5 as ap

---
+++ src/Acmicpc/As/B5/ThirtyThousandth/Four/Four/EDaysOreCartPull34446.java
@@ -0,0 +1,17 @@
+package Acmicpc.As.B5.ThirtyThousandth.Four.Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EDaysOreCartPull34446 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int m = Integer.parseInt(br.readLine());
+		br.readLine();
+		br.readLine();
+		
+		System.out.println(m*2);
+		br.close();
+	}
+}
