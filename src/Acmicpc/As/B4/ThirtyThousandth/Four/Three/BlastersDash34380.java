f4b3624bda1b79f3431cfa6518a720f0cc3e5eec
powerseunghun <size411@naver.com>
2025-09-26 17:56:25 +0900

    b4 as ap

---
+++ src/Acmicpc/As/B4/ThirtyThousandth/Four/Three/BlastersDash34380.java
@@ -0,0 +1,17 @@
+package Acmicpc.As.B4.ThirtyThousandth.Four.Three;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BlastersDash34380 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		int n = Integer.parseInt(br.readLine());
+		br.readLine();
+		
+		System.out.println((20+n) * 2);
+		br.close();
+	}
+}
