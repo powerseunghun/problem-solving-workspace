122a2f293392ed9a5f28db2f48d61e4b4eeceedc
powerseunghun <size411@naver.com>
2025-10-08 19:17:08 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B4/ThirtyThousandth/Three/Six/TestingLED33652.java
@@ -0,0 +1,28 @@
+package Acmicpc.As.B4.ThirtyThousandth.Three.Six;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TestingLED33652 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()), M = 0, O = 0, res = Integer.MAX_VALUE;
+		String str = null;
+		boolean flag = true;
+		
+		while(N-- > 0) {
+			str = br.readLine();
+			M = Integer.parseInt(str.split(" ")[0]);
+			O = Integer.parseInt(str.split(" ")[1]);
+			
+			if (O == 0) {
+				flag = false;
+				res = Math.min(res, M);
+			}
+		}
+		
+		System.out.println(flag ? -1 : res);
+		br.close();
+	}
+}
