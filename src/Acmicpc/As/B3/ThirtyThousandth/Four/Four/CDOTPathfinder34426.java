f8be48f8b0a33331529419d7dc0852c9340c80fb
powerseunghun <size411@naver.com>
2025-10-23 20:57:54 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/ThirtyThousandth/Four/Four/CDOTPathfinder34426.java
@@ -0,0 +1,37 @@
+package Acmicpc.As.B3.ThirtyThousandth.Four.Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class CDOTPathfinder34426 {
+	static Map<Double, Integer> map = new HashMap<>();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine()), N = 0;
+		double a = 0, b = 0, min = 0;
+		StringTokenizer st = null;
+		String str = null;
+		
+		while(T-- > 0) {
+			map.clear();
+			min = Double.MAX_VALUE;
+			N = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				a = Double.parseDouble(st.nextToken());
+				b = Double.parseDouble(st.nextToken());
+				map.put(b/a, i+1);
+				min = Math.min(b/a, min);
+			}
+			sb.append(map.get(min)).append("\n");
+		}
+		
+		System.out.print(sb.toString());
+		br.close();
+	}
+}
