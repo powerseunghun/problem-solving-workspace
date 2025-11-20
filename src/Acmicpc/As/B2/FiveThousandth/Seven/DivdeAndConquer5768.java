b396e06d25ee972e913d7927e42691984a40a501
powerseunghun <size411@naver.com>
2025-11-20 17:54:05 +0900

    b2 as ap

---
+++ src/Acmicpc/As/B2/FiveThousandth/Seven/DivdeAndConquer5768.java
@@ -0,0 +1,24 @@
+package Acmicpc.As.B2.FiveThousandth.Seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DivdeAndConquer5768 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+		String str = null;
+		int M = 0, N = 0;
+		
+		while(true) {
+			str = br.readLine();
+			M = Integer.parseInt(str.split(" ")[0]);
+			N = Integer.parseInt(str.split(" ")[1]);
+			
+			if (M == 0 && N == 0) break;
+			// -- Pair c++
+		}
+		br.close();
+	}
+}
