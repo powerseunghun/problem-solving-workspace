953df4b0cbf6e09da2c4586acf280898d4d983fe
powerseunghun <size411@naver.com>
2025-12-05 17:42:43 +0900

    b2 as ap

---
+++ src/Acmicpc/As/B2/TenThousandth/Eight/One/WhatAnEasyProblem18158.java
@@ -0,0 +1,14 @@
+package Acmicpc.As.B2.TenThousandth.Eight.One;
+
+public class WhatAnEasyProblem18158 {
+//	#include "WAEP.h"
+//
+//	void init(int T)
+//	{
+//	}
+//
+//	int janken(int P)
+//	{
+//		return P;
+//	}
+}
