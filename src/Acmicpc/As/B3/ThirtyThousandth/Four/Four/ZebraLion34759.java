363fcf473ef5702e024fa36402f526abad3ad59f
powerseunghun <size411@naver.com>
2026-01-07 18:32:25 +0900

    b3 as ap

---
+++ src/Acmicpc/As/B3/ThirtyThousandth/Four/Four/ZebraLion34759.java
@@ -0,0 +1,15 @@
+package Acmicpc.As.B3.ThirtyThousandth.Four.Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ZebraLion34759 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Long.parseLong(br.readLine());
+		
+		System.out.println(N*2);
+		br.close();
+	}
+}
