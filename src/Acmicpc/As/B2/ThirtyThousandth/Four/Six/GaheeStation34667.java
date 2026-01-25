7ea7dd4ef8fd3840980f98a48b356196f5ab0e05
powerseunghun <size411@naver.com>
2026-01-25 20:41:08 +0900

    b2 as ap

---
+++ src/Acmicpc/As/B2/ThirtyThousandth/Four/Six/GaheeStation34667.java
@@ -0,0 +1,16 @@
+package Acmicpc.As.B2.ThirtyThousandth.Four.Six;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GaheeStation34667 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		br.readLine();
+		
+		System.out.println(str.charAt(0) + str);
+		br.close();
+	}
+}
