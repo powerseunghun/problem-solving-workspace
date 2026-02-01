0344ddec9c3d7435899658db93b8b458aecd41bb
powerseunghun <size411@naver.com>
2026-02-01 18:17:00 +0900

    b2 as ap mod

--- src/Acmicpc/As/B2/FourThousandth/Four/ElfSword4436.java
+++ src/Acmicpc/As/B2/FourThousandth/Four/ElfSword4436.java
@@ -3,31 +3,41 @@ package Acmicpc.As.B2.FourThousandth.Four;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ElfSword4436 {
 	static final int target = 10;
-	static Set<Character> set = new HashSet<>();
-	static void func(long N) {
-		String str = String.valueOf(N);
-		for (int i = 0; i < str.length(); i++) {
-			set.add(str.charAt(i));
+	static boolean[] arr = null;
+	static int getCount(boolean[] arr) {
+		int res = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i]) {
+				res++;
+			}
 		}
+		return res;
 	}
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		String tmp = null;
-		long N = 0;
+		long N = 0, T = 0;
 		
 		while ((tmp = br.readLine()) != null) {
-			set.clear();
-			tmp = br.readLine();
 			N = Integer.parseInt(tmp);
+			arr = new boolean[10];
+			Arrays.fill(arr, false);
 			for (int k = 1; ; k++) {
-				func(N * k);
-				if (set.size() == target) {
+				T = N * k;
+				
+				while(T != 0) {
+					arr[(int) (T%10)] = true;
+					T /= 10;
+				}
+				
+				if (getCount(arr) == target) {
 					sb.append(k).append("\n");
 					break;
 				}
