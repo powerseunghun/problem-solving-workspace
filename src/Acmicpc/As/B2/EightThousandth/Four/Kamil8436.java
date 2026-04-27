649d95afaeb389bcba5d4561490ef5d9597b8087
powerseunghun <size411@naver.com>
2026-04-27 14:33:38 +0900

    b2 as ap

--- src/Acmicpc/As/B2/EightThousandth/Four/Kamil8436.java
+++ src/Acmicpc/As/B2/EightThousandth/Four/Kamil8436.java
@@ -7,7 +7,6 @@ public class Kamil8436 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
-		int t = 0, d = 0, l = 0, f = 0;
 		long res = 0;
 		
 		for (int i = 0; i < str.length(); i++) {
